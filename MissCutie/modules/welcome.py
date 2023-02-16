4729c446c6cea611156a29a460695bcc7540a13c
SAIF ALI <sachinsaif03@gmail.com>
2023-02-16 08:59:09 +0530

    Update welcome.py

--- MissCutie/modules/welcome.py
+++ MissCutie/modules/welcome.py
@@ -1107,7 +1107,7 @@ dispatcher.add_handler(CLEAN_SERVICE_HANDLER)
 dispatcher.add_handler(BUTTON_VERIFY_HANDLER)
 dispatcher.add_handler(WELCOME_MUTE_HELP)
 
-__mod_name__ = "Welcome⭐"
+__mod_name__ = "Welcome"
 __command_list__ = []
 __handlers__ = [
     NEW_MEM_HANDLER,
