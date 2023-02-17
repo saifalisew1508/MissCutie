1a0bc40d053db82b0c8103c83c2d1fb6742c7324
SAIF ALI <sachinsaif03@gmail.com>
2023-02-17 16:47:50 +0530

    Update __main__.py

--- MissCutie/__main__.py
+++ MissCutie/__main__.py
@@ -72,7 +72,7 @@ def get_readable_time(seconds: int) -> str:
 
 
 PM_START_TEXT = """
-*Hey* {} there! My name is *{}* - I'm here to help you manage your groups! Hit /help to find out more about how to use me to my full potential.
+*Hey* [{member.user['first_name']}](tg://user?id={member.user['id']}) there! My name is *{}* - I'm here to help you manage your groups! Hit /help to find out more about how to use me to my full potential.
 
 Join my [updates channel](http://t.me/MissCutieUpdates) to get information on all the latest updates.
 
@@ -366,8 +366,7 @@ def saif_about_callback(update: Update, context: CallbackContext):
     if query.data == "saif_":
         uptime = get_readable_time((time.time() - StartTime))
         query.message.edit_text(
-            text=f"""
-            *Hey {}, My name is {dispatcher.bot.first_name}*"
+            text=f"*Hey {}, My name is {dispatcher.bot.first_name}*"
             "\n*This is a Bot developed in Python using Telethon, Pyrogram and PTB Library. it's online since 09 January 2022 and it's constantly updated!*"
             "\n*Written in Python with SQLalchemy and MongoDB as database.*"
             "\n\nAbout bot Library, Databse Users and Chats"
@@ -387,8 +386,7 @@ def saif_about_callback(update: Update, context: CallbackContext):
             "\n➲  /mstart  to start music bot."
             "\n➲  /mhelp  to get  all music help  button."
             "\n➲  /malive  to cheak  music bot alive or  fumked."
-            f"\n\n➻ click on the buttons given below for getting basic help and info about {dispatcher.bot.first_name}.
-            """",
+            f"\n\n➻ click on the buttons given below for getting basic help and info about {dispatcher.bot.first_name}.",
             parse_mode=ParseMode.MARKDOWN,
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
