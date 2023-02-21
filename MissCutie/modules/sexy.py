e22324716479e19c8129bc40157735fb9e28897d
SAIF ALI <sachinsaif03@gmail.com>
2023-02-21 06:48:55 +0530

    Update sexy.py

--- MissCutie/modules/sexy.py
+++ MissCutie/modules/sexy.py
@@ -4,7 +4,7 @@ from telethon import Button, events
 
 from .. import telethn as asst
 
-BUTTON = [[Button.url("🍒 support 🍒", "https://t.me/the_support_chat")]]
+BUTTON = [[Button.url("Support", "https://t.me/MissCutie_Support")]]
 HOT = "https://telegra.ph/file/daad931db960ea40c0fca.gif"
 SMEXY = "https://telegra.ph/file/a23e9fd851fb6bc771686.gif"
 LEZBIAN = "https://telegra.ph/file/5609b87f0bd461fc36acb.gif"
