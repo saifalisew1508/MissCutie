bd8a6f18fa2eb68c7aea7a743ce3b2915e0b2b81
SAIF ALI <sachinsaif03@gmail.com>
2023-02-22 21:09:00 +0530

    Update userinfo.py

--- MissCutie/modules/userinfo.py
+++ MissCutie/modules/userinfo.py
@@ -337,15 +337,15 @@ def info(update: Update, context: CallbackContext):
                     [
                         [
                             InlineKeyboardButton(
-                                "developer", url="https://t.me/itz_mst_boi"
+                                "Developer", url="https://t.me/PrinceXofficial"
                             ),
                             InlineKeyboardButton(
-                                "Disaster", url="https://t.me/MissCutie_Support/26"
+                                "Support", url="https://t.me/MissCutie_Support"
                             ),
                         ],
                         [
                             InlineKeyboardButton(
-                                text="➕add me to your group➕",
+                                text="➕ Add Me To Your Group ➕",
                                 url=f"https://t.me/groupcontrollertgbot?startgroup=true",
                             ),
                         ],
@@ -366,7 +366,7 @@ def info(update: Update, context: CallbackContext):
                                 "Developer", url="https://t.me/PrinceXofficial"
                             ),
                             InlineKeyboardButton(
-                                "Disaster", url="https://t.me/MissCutie_Support/26"
+                                "Support", url="https://t.me/MissCutie_Support"
                             ),
                         ],
                         [
