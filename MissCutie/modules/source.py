371f5e09c6867b8d6e7539718dcadcf82cab4c6f
SAIF ALI <sachinsaif03@gmail.com>
2023-02-23 09:54:34 +0530

    Update source.py

--- MissCutie/modules/source.py
+++ MissCutie/modules/source.py
@@ -6,7 +6,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram import __version__ as o
 from telethon import __version__ as s
 
-from MissCutie import OWNER_ID, dispatcher
+from MissCutie import OWNER_ID, BOT_NAME, dispatcher
 from MissCutie import pbot as client
 
 saif = "https://te.legra.ph/file/5196d5fa658145cb6b9ef.jpg"
@@ -16,21 +16,22 @@ saif = "https://te.legra.ph/file/5196d5fa658145cb6b9ef.jpg"
 async def repo(client, message):
     await message.reply_photo(
         photo=saif,
-        caption=f"""**hey​ {message.from_user.mention()},\n\ni am [{dispatcher.bot.first_name}](t.me/{dispatcher.bot.username})**
+        caption=f"""**Hey {message.from_user.mention()},\n\ni am [{dispatcher.bot.first_name}](t.me/{dispatcher.bot.username})**
 
-**➥ My Developer​ :** @PrinceXofficial
+**➥ My Developer :** @PrinceXofficial
 **➥ Python Version :** `{y()}`
 **➥ Library Version :** `{o}` 
 **➥ Telethon Version :** `{s}` 
 **➥ Pyrogram Version :** `{z}`
 
-**group ✘ controller source is now public and now you can make your own bot.**
+**{BOT_NAME} source is now public and now you can make your own bot.**
 """,
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "Developer",f"tg://user?id={OWNER_ID}"
+                        "Developer",
+                        url="t.me/PrinceXofficial",
                     ),
                     InlineKeyboardButton(
                         "Source",
@@ -42,7 +43,7 @@ async def repo(client, message):
     )
 
 
-__mod_name__ = "Repo✦"
+__mod_name__ = "Repo"
 _help__ = """ /repo to get repo 
              /Source to get repo
 """
