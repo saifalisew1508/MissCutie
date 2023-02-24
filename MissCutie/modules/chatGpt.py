4d856a6461073701ffd811234d0b0487b456e8e7
SAIF ALI <sachinsaif03@gmail.com>
2023-02-24 07:51:08 +0530

    Update chatGpt.py

--- MissCutie/modules/chatGpt.py
+++ MissCutie/modules/chatGpt.py
@@ -11,7 +11,7 @@ openai.api_key = "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"
 
 
 # Your GPT model ID
-model_id = "turingbotai/GPT3_India"
+model_id = "turingbotai"
 
 # The maximum number of tokens to generate
 max_tokens = 400
