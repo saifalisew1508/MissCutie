d9e74f18234406b4e84a91a0adba95ce8a88f905
SAIF ALI <sachinsaif03@gmail.com>
2023-02-27 23:20:32 +0530

    Update afk.py

--- MissCutie/modules/afk.py
+++ MissCutie/modules/afk.py
@@ -30,7 +30,7 @@ def get_readable_time(seconds: int) -> str:
     ping_time += ":".join(time_list)
     return ping_time
 
-@app.on_message(filters.command(["afk", f"afk@szrosebot"]))
+@app.on_message(filters.command(["afk", f"afk@MissCutieRobot"]))
 async def going_afk(_, message: Message):
     if message.sender_chat:
         return
