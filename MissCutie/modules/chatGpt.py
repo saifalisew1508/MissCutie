0a18e93140da39fc8bfc498a4ff4f1dd80f00f79
Saif Ali <sachinsaif03@gmail.com>
2023-06-01 15:44:23 +0530

    Update chatGpt.py

--- MissCutie/modules/chatGpt.py
+++ MissCutie/modules/chatGpt.py
@@ -1,30 +1,44 @@
-from telethon import TelegramClient, events
-from openai import ChatCompletionClient
-import asyncio
-
-
+import openai
+from telethon import Telegramtelethn, events
 from MissCutie import telethn, BOT_NAME
 
-# Your OpenAI API key
-chatgpt = ChatCompletionClient(api_key='4rde2g7cprXOamWae3eMT3BlbkFJQ5yINbloY7KISJuN2DC7')
+# Set up your OpenAI API credentials
+openai.api_key = '4rde2g7cprXOamWae3eMT3BlbkFJQ5yINbloY7KISJuN2DC7'
 
 
-# Register the event handler for incoming messages
-@telethn.on(events.NewMessage(pattern="^[!/]chat ?(.*)"))
-async def handle_new_message(event):
+
+# Define the event handler for incoming messages
+@telethn.on(events.NewMessage(pattern='/ask', incoming=True))
+async def handle_ask_command(event):
     if not event.is_private:
         return
 
     message = event.message.message
     sender_id = event.sender_id
 
-    # Process the message using ChatGPT
-    response = chatgpt.complete(message, sender_id=sender_id)
+    # Extract the question from the message
+    question = message.replace('/ask', '').strip()
 
-    # Send the response back to the user
-    await event.respond(response.choices[0].text)
+    if question:
+        # Process the question using OpenAI GPT-3
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": question}
+            ],
+            conversation_id=str(sender_id)
+        )
 
+        # Extract the generated response from the OpenAI API
+        answer = response.choices[0].message.content
 
+        # Send the response back to the user
+        await event.respond(answer)
+    else:
+        # If no question is provided, send a help message
+        help_message = "Please provide a question. Usage: /ask [your question]"
+        await event.respond(help_message)
 
 
 __help__ = f"""
