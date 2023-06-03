ea118564d03f1b55ca1b64c981f101953065607e
Saif Ali <sachinsaif03@gmail.com>
2023-06-03 17:02:21 +0530

    Add files via upload

--- MissCutie/modules/youtube.py
+++ MissCutie/modules/youtube.py
@@ -1,12 +1,18 @@
+from __future__ import unicode_literals
+
 import asyncio
 import os
 import time
 from urllib.parse import urlparse
 
+
+
 import wget
 from pyrogram import filters
 from pyrogram.types import Message
 from youtubesearchpython import SearchVideos
+from yt_dlp import YoutubeDL
+
 import requests
 import yt_dlp
 from youtube_search import YoutubeSearch
@@ -21,70 +27,68 @@ def time_to_seconds(time):
 
 @pbot.on_message(filters.command(["song", "music"]))
 def song(client, message):
+
+    message.delete()
+    user_id = message.from_user.id
+    user_name = message.from_user.first_name
+    chutiya = "[" + user_name + "](tg://user?id=" + str(user_id) + ")"
+
+    query = ""
+    for i in message.command[1:]:
+        query += " " + str(i)
+    print(query)
+    m = message.reply("**Searching,Please wait...**")
+    ydl_opts = {"format": "bestaudio[ext=m4a]"}
     try:
-        message.delete()
-        user_id = message.from_user.id
-        user_name = message.from_user.first_name
-        chutiya = "[" + user_name + "](tg://user?id=" + str(user_id) + ")"
+        results = YoutubeSearch(query, max_results=1).to_dict()
+        link = f"https://youtube.com{results[0]['url_suffix']}"
+        # print(results)
+        title = results[0]["title"][:40]
+        thumbnail = results[0]["thumbnails"][0]
+        thumb_name = f"thumb{title}.jpg"
+        thumb = requests.get(thumbnail, allow_redirects=True)
+        open(thumb_name, "wb").write(thumb.content)
 
-        query = ""
-        for i in message.command[1:]:
-            query += " " + str(i)
-        print(query)
-        m = message.reply("**Searching, please wait...**")
-        ydl_opts = {"format": "bestaudio[ext=m4a]"}
-        try:
-            results = YoutubeSearch(query, max_results=1).to_dict()
-            link = f"https://youtube.com{results[0]['url_suffix']}"
-            # print(results)
-            title = results[0]["title"][:40]
-            thumbnail = results[0]["thumbnails"][0]
-            thumb_name = f"thumb{title}.jpg"
-            thumb = requests.get(thumbnail, allow_redirects=True)
-            open(thumb_name, "wb").write(thumb.content)
-
-            duration = results[0]["duration"]
-            results[0]["url_suffix"]
-            views = results[0]["views"]
-
-        except Exception as e:
-            m.edit("**Song not found on YouTube.**")
-            print(str(e))
-            return
-        m.edit("Downloading...\nPlease wait...")
-        try:
-            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-                info_dict = ydl.extract_info(link, download=False)
-                audio_file = ydl.prepare_filename(info_dict)
-                ydl.process_info(info_dict)
-            rep = f"**Title :** {title[:25]}\n**Duration :** `{duration}`\n**Views :** `{views}`\n**Requested By :** {chutiya}"
-            secmul, dur, dur_arr = 1, 0, duration.split(":")
-            for i in range(len(dur_arr) - 1, -1, -1):
-                dur += int(dur_arr[i]) * secmul
-                secmul *= 60
-            message.reply_audio(
-                audio_file,
-                caption=rep,
-                thumb=thumb_name,
-                title=title,
-                duration=dur,
-            )
-            m.delete()
-        except Exception as e:
-            m.edit(
-                f"**Downloading error, report this error at [support chat](t.me/{SUPPORT_CHAT}) 💕**\n\**error :** {e}"
-            )
-            print(e)
-
-        try:
-            os.remove(audio_file)
-            os.remove(thumb_name)
-        except Exception as e:
-            print(e)
+        duration = results[0]["duration"]
+        results[0]["url_suffix"]
+        views = results[0]["views"]
 
     except Exception as e:
-        print(f"An error occurred: {str(e)}")
+        m.edit(
+            "**Song not found on youtube.**"
+        )
+        print(str(e))
+        return
+    m.edit("Downloading...\nPlease Wait...")
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(link, download=False)
+            audio_file = ydl.prepare_filename(info_dict)
+            ydl.process_info(info_dict)
+        rep = f"**Title : ** {title[:25]}\n**Duration : ** `{duration}`\n**Views : ** `{views}`\n**Requested By : ** {chutiya}"
+        secmul, dur, dur_arr = 1, 0, duration.split(":")
+        for i in range(len(dur_arr) - 1, -1, -1):
+            dur += int(dur_arr[i]) * secmul
+            secmul *= 60
+        message.reply_audio(
+            audio_file,
+            caption=rep,
+            thumb=thumb_name,
+            title=title,
+            duration=dur,
+        )
+        m.delete()
+    except Exception as e:
+        m.edit(
+            f"**Downloading error, report this error at [support chat](t.me/{SUPPORT_CHAT}) 💕**\n\**error :** {e}"
+        )
+        print(e)
 
+    try:
+        os.remove(audio_file)
+        os.remove(thumb_name)
+    except Exception as e:
+        print(e)
 
 # video tool
 
@@ -110,77 +114,73 @@ def get_text(message: Message) -> [None, str]:
 
 @pbot.on_message(filters.command(["vsong", "video"]))
 async def ytmusic(client, message: Message):
-    try:
-        urlissed = get_text(message)
-        await message.delete()
-        user_id = message.from_user.id
-        user_name = message.from_user.first_name
-        chutiya = "[" + user_name + "](tg://user?id=" + str(user_id) + ")"
+    urlissed = get_text(message)
+    await message.delete()
+    user_id = message.from_user.id
+    user_name = message.from_user.first_name
+    chutiya = "[" + user_name + "](tg://user?id=" + str(user_id) + ")"
 
-        pablo = await client.send_message(message.chat.id, f"Searching, please wait...")
-        if not urlissed:
-            await pablo.edit("Sorry, song not found on YouTube.")
-            return
-
-        search = SearchVideos(f"{urlissed}", offset=1, mode="dict", max_results=1)
-        mi = search.result()
-        mio = mi["search_result"]
-        mo = mio[0]["link"]
-        thum = mio[0]["title"]
-        fridayz = mio[0]["id"]
-        thums = mio[0]["channel"]
-        kekme = f"https://img.youtube.com/vi/{fridayz}/hqdefault.jpg"
-        await asyncio.sleep(0.6)
-        url = mo
-        sedlyf = wget.download(kekme)
-        opts = {
-            "format": "best",
-            "addmetadata": True,
-            "key": "FFmpegMetadata",
-            "prefer_ffmpeg": True,
-            "geo_bypass": True,
-            "nocheckcertificate": True,
-            "postprocessors": [{"key": "FFmpegVideoConvertor", "preferedformat": "mp4"}],
-            "outtmpl": "%(id)s.mp4",
-            "logtostderr": False,
-            "quiet": True,
-        }
-        try:
-            with yt_dlp.YoutubeDL(opts) as ytdl:
-                infoo = ytdl.extract_info(url, False)
-                round(infoo["duration"] / 60)
-                ytdl_data = ytdl.extract_info(url, download=True)
-
-        except Exception as e:
-            await pablo.edit(f"**Failed to download.**\n**Error:** `{str(e)}`")
-            return
-        c_time = time.time()
-        file_stark = f"{ytdl_data['id']}.mp4"
-        capy = f"❄ **Title :** [{thum}]({mo})\n💫 **Channel Name :** {thums}\n✨ **Searched :** {urlissed}\n🥀 **Requested By :** {chutiya}"
-        await client.send_video(
-            message.chat.id,
-            video=open(file_stark, "rb"),
-            duration=int(ytdl_data["duration"]),
-            file_name=str(ytdl_data["title"]),
-            thumb=sedlyf,
-            caption=capy,
-            supports_streaming=True,
-            progress_args=(
-                pablo,
-                c_time,
-                f"Please wait...\n\nUploading `{urlissed}` from YouTube servers...💫",
-                file_stark,
-            ),
+    pablo = await client.send_message(message.chat.id, f"searching, please wait...")
+    if not urlissed:
+        await pablo.edit(
+            "Sorry Song Not Found On Youtube."
         )
-        await pablo.delete()
-        for files in (sedlyf, file_stark):
-            if files and os.path.exists(files):
-                os.remove(files)
+        return
+
+    search = SearchVideos(f"{urlissed}", offset=1, mode="dict", max_results=1)
+    mi = search.result()
+    mio = mi["search_result"]
+    mo = mio[0]["link"]
+    thum = mio[0]["title"]
+    fridayz = mio[0]["id"]
+    thums = mio[0]["channel"]
+    kekme = f"https://img.youtube.com/vi/{fridayz}/hqdefault.jpg"
+    await asyncio.sleep(0.6)
+    url = mo
+    sedlyf = wget.download(kekme)
+    opts = {
+        "format": "best",
+        "addmetadata": True,
+        "key": "FFmpegMetadata",
+        "prefer_ffmpeg": True,
+        "geo_bypass": True,
+        "nocheckcertificate": True,
+        "postprocessors": [{"key": "FFmpegVideoConvertor", "preferedformat": "mp4"}],
+        "outtmpl": "%(id)s.mp4",
+        "logtostderr": False,
+        "quiet": True,
+    }
+    try:
+        with YoutubeDL(opts) as ytdl:
+            infoo = ytdl.extract_info(url, False)
+            round(infoo["duration"] / 60)
+            ytdl_data = ytdl.extract_info(url, download=True)
 
     except Exception as e:
-        print(f"An error occurred: {str(e)}")
-
-
+        await pablo.edit(f"**failed to download.** \n**error :** `{str(e)}`")
+        return
+    c_time = time.time()
+    file_stark = f"{ytdl_data['id']}.mp4"
+    capy = f"❄ **Title :** [{thum}]({mo})\n💫 **Channel Name :** {thums}\n✨ **Searched :** {urlissed}\n🥀 **Requested By :** {chutiya}"
+    await client.send_video(
+        message.chat.id,
+        video=open(file_stark, "rb"),
+        duration=int(ytdl_data["duration"]),
+        file_name=str(ytdl_data["title"]),
+        thumb=sedlyf,
+        caption=capy,
+        supports_streaming=True,
+        progress_args=(
+            pablo,
+            c_time,
+            f"Please wait...\n\nUploading `{urlissed}` from youtube servers...💫",
+            file_stark,
+        ),
+    )
+    await pablo.delete()
+    for files in (sedlyf, file_stark):
+        if files and os.path.exists(files):
+            os.remove(files)
 
 
 __mod_name__ = "YouTube"
