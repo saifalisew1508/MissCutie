f64a2ba4c20b7acd2c4d529b0b986900b86b7d45
Saif Ali <sachinsaif03@gmail.com>
2023-06-03 15:58:02 +0530

    Update yt.py

--- MissCutie/modules/yt.py
+++ MissCutie/modules/yt.py
@@ -2,8 +2,8 @@ import youtube_dl
 from telethon.sync import TelegramClient
 from telethon import events
 from telethon.tl.types import InputFile
+from youtubesearchpython import VideosSearch
 from MissCutie import telethn as client
-
 
 ydl_opts = {
     'format': 'bestaudio/best',
@@ -15,11 +15,11 @@ ydl_opts = {
 }
 
 
-@client.on(events.NewMessage(pattern='/yt'))
+@client.on(events.NewMessage(pattern='/song'))
 async def handle_song(event):
     chat = await event.get_chat()
     message = event.message
-    song_name = message.text.replace('/yt', '').strip()
+    song_name = message.text.replace('/song', '').strip()
 
     if not song_name:
         await event.respond('Please provide a song name.')
@@ -27,18 +27,25 @@ async def handle_song(event):
 
     query = f'{song_name} lyrics'
     try:
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            info = ydl.extract_info(query, download=False)
-            url = info['url']
-            title = info['title']
+        videos = VideosSearch(query, limit=1).result()['result']
+        if len(videos) > 0:
+            video = videos[0]
+            url = video['link']
+            title = video['title']
             filename = f'{title}.mp3'
-            ydl.download([url])
 
-        with open(filename, 'rb') as f:
-            result = await client.upload_file(f)
-            audio_input_file = InputFile(result.file_id, result.parts, result.name)
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                info = ydl.extract_info(url, download=False)
+                ydl.download([url])
 
-        await client.send_file(chat, file=audio_input_file, caption=title)
-        await event.respond('Song downloaded and sent.')
+            with open(filename, 'rb') as f:
+                result = await client.upload_file(f)
+                audio_input_file = InputFile(result.file_id, result.parts, result.name)
+
+            await client.send_file(chat, file=audio_input_file, caption=title)
+            await event.respond('Song downloaded and sent.')
+        else:
+            await event.respond('No search results found for the given song name.')
     except Exception as e:
         await event.respond(f'An error occurred: {str(e)}')
+
