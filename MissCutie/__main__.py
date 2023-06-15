fea6210dcf33850d5038228838bca6435b7a7d7d
Saif Ali <sachinsaif03@gmail.com>
2023-06-15 23:01:21 +0530

    Update __main__.py

--- MissCutie/__main__.py
+++ MissCutie/__main__.py
@@ -994,5 +994,5 @@ Made By @PrinceXofficial
 if __name__ == "__main__":
     LOGGER.info("Successfully loaded modules: " + str(ALL_MODULES))
     telethn.start(bot_token=TOKEN)
- 
+    pbot.start()
     main()
