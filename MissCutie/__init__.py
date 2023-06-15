9e07645cab67c47d0b6936f0e09dba799b5baed1
Saif Ali <sachinsaif03@gmail.com>
2023-06-15 18:32:50 +0530

    Update __init__.py

--- MissCutie/__init__.py
+++ MissCutie/__init__.py
@@ -48,6 +48,9 @@ logging.basicConfig(
     level=LOGGER_LEVEL,
 )
 
+logging.getLogger("apscheduler").setLevel(logging.ERROR)
+logging.getLogger("telethon").setLevel(logging.ERROR)
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
 LOGGER = logging.getLogger(__name__)
 
 # if version < 3.9, stop bot.
