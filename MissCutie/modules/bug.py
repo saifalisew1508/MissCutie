153c8f90361f5df7fabf560a21dccc028b22010e
Saif Ali <sachinsaif03@gmail.com>
2023-06-15 19:28:04 +0530

    Update

--- MissCutie/modules/bug.py
+++ MissCutie/modules/bug.py
@@ -1,11 +1,14 @@
 from datetime import datetime
 
 from pyrogram import filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+from pyrogram.enums import ChatType
+from pyrogram.types import (
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    Message,
+)
 
-from MissCutie import OWNER_ID as owner_id
-from MissCutie import SUPPORT_CHAT as log
-from MissCutie import pbot as Client
+from MissCutie import pbot, OWNER_ID, OWNER_USERNAME, SUPPORT_CHAT
 from MissCutie.utils.errors import capture_err
 
 
@@ -14,106 +17,87 @@ def content(msg: Message) -> [None, str]:
 
     if msg.text is None:
         return None
-    if " " in text_to_return:
-        try:
-            return msg.text.split(None, 1)[1]
-        except IndexError:
-            return None
-    else:
+    if " " not in text_to_return:
+        return None
+    try:
+        return msg.text.split(None, 1)[1]
+    except IndexError:
         return None
 
 
-@Client.on_message(filters.command("bug"))
+@pbot.on_message(filters.command("bug"))
 @capture_err
 async def bug(_, msg: Message):
     if msg.chat.username:
-        chat_username = f"@{msg.chat.username}/`{msg.chat.id}`"
+        chat_username = (f"@{msg.chat.username} / `{msg.chat.id}`")
     else:
-        chat_username = f"Private Group/`{msg.chat.id}`"
+        chat_username = (f"Private Group / `{msg.chat.id}`")
 
     bugs = content(msg)
     user_id = msg.from_user.id
     mention = (
-        "[" + msg.from_user.first_name + "](tg://user?id=" + str(msg.from_user.id) + ")"
-    )
+        f"[{msg.from_user.first_name}](tg://user?id={str(msg.from_user.id)}" +
+        ")")
+
     datetimes_fmt = "%d-%m-%Y"
     datetimes = datetime.utcnow().strftime(datetimes_fmt)
 
-    thumb = "https://telegra.ph/file/ce00e06eeeec173a51ba5.mp4"
-
     bug_report = f"""
-**#bug : ** **tg://user?id={owner_id}**
+**#BUG : ** **@{OWNER_USERNAME}**
+**From User : ** **{mention}**
+**User ID : ** **{user_id}**
+**Group : ** **{chat_username}**
+**Bug Report : ** **{bugs}**
+**Event Stamp : ** **{datetimes}**"""
 
-**reported By : ** **{mention}**
-**user id : ** **{user_id}**
-**chat : ** **{chat_username}**
-
-**bug : ** **{bugs}**
-
-**event stamp : ** **{datetimes}**"""
-
-    if msg.chat.type == "private":
-        await msg.reply_text("<b>➥ This command only for groups.</b>")
+    if msg.chat.type == ChatType.PRIVATE:
+        await msg.reply_text("❎ <b>This command only works in groups.</b>")
         return
 
-    if user_id == owner_id:
+    if user_id == OWNER_ID:
         if bugs:
             await msg.reply_text(
-                "<b>➥ Are you comedy me 🤣?, You are owner of the bot.</b>",
-            )
+                "❎ <b>How can be owner bot reporting bug??</b>", )
             return
-        else:
-            await msg.reply_text("LuL OWNER!")
-    elif user_id != owner_id:
-        if bugs:
-            await msg.reply_text(
-                f"<b>Bug report : {bugs}</b>\n\n"
-                "<b>➥ Bug successfully reported at support chat !</b>",
-                reply_markup=InlineKeyboardMarkup(
-                    [[InlineKeyboardButton("• Close •", callback_data=f"close_reply")]]
-                ),
-            )
-            await Client.send_photo(
-                log,
-                photo=thumb,
-                caption=f"{bug_report}",
-                reply_markup=InlineKeyboardMarkup(
-                    [
-                        [InlineKeyboardButton("• View Bug •", url=f"{msg.link}")],
-                        [
-                            InlineKeyboardButton(
-                                "• close •", callback_data="close_send_photo"
-                            )
-                        ],
-                    ]
-                ),
-            )
-        else:
-            await msg.reply_text(
-                f"<b>➥ No Bug To Report !</b>",
-            )
+        await msg.reply_text("Owner noob!")
+    elif bugs:
+        await msg.reply_text(
+            f"<b>Bug Report : {bugs}</b>\n\n"
+            "✅ <b>The bug was successfully reported to the support group!</b>",
+            reply_markup=InlineKeyboardMarkup(
+                [[InlineKeyboardButton("Close",
+                                       callback_data="close_reply")]]),
+        )
+
+        thumb = "https://i.pinimg.com/564x/f2/47/8b/f2478ba4e193470ebcdf61a2ad0f33ce.jpg"
+
+        await pbot.send_photo(
+            SUPPORT_CHAT,
+            photo=thumb,
+            caption=f"{bug_report}",
+            reply_markup=InlineKeyboardMarkup(
+                [[InlineKeyboardButton("➡ View Bug", url=f"{msg.link}")],
+                 [
+                     InlineKeyboardButton("❌ Close",
+                                          callback_data="close_send_photo")
+                 ]]))
+    else:
+        await msg.reply_text("❎ <b>No bug to Report!</b>")
 
 
-@Client.on_callback_query(filters.regex("close_reply"))
-async def close_reply(msg, CallbackQuery):
+@pbot.on_callback_query(filters.regex("close_reply"))
+async def close_reply(CallbackQuery):
     await CallbackQuery.message.delete()
 
 
-@Client.on_callback_query(filters.regex("close_send_photo"))
+@pbot.on_callback_query(filters.regex("close_send_photo"))
 async def close_send_photo(_, CallbackQuery):
-    is_Admin = await Client.get_chat_member(
-        CallbackQuery.message.chat.id, CallbackQuery.from_user.id
-    )
+    is_Admin = await pbot.get_chat_member(CallbackQuery.message.chat.id,
+                                           CallbackQuery.from_user.id)
     if not is_Admin.can_delete_messages:
-        return await CallbackQuery.answer(
-            "You haven't right to close this.", show_alert=True
-        )
-    else:
-        await CallbackQuery.message.delete()
+        return await CallbackQuery.answer("You're not allowed to close this.",
+                                          show_alert=True)
+    await CallbackQuery.message.delete()
 
 
-__help__ = """
-*For reporting any bug*
- ➥ /bug *:* To rport a bug at support chat.
-"""
-__mod_name__ = "Bugs"
+__mod_name__ = "Bug"
