2ecfb8a955398b46ee3bc3798a5fa0adcc42c066
Saif Ali <sachinsaif03@gmail.com>
2023-06-18 13:06:47 +0530

    Update purge.py

--- MissCutie/modules/purge.py
+++ MissCutie/modules/purge.py
@@ -96,20 +96,11 @@ async def delete_messages(event):
         pass
 
 
-
-__help__ = """
-*Admin only:*
- - /del: deletes the message you replied to
- - /purge: deletes all messages between this and the replied to message.
- - /purge <integer X>: deletes the replied message, and X messages following it if replied to a message.
-"""
-
 PURGE_HANDLER = purge_messages, events.NewMessage(pattern="^[!/]purge$")
 DEL_HANDLER = delete_messages, events.NewMessage(pattern="^[!/]del$")
 
 telethn.add_event_handler(*PURGE_HANDLER)
 telethn.add_event_handler(*DEL_HANDLER)
 
-__mod_name__ = "Purges"
 __command_list__ = ["del", "purge"]
 __handlers__ = [PURGE_HANDLER, DEL_HANDLER]
