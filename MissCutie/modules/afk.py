43de0b85670958a54748cd0e2cfaf90dfa06fce2
Saif Ali <sachinsaif03@gmail.com>
2023-06-21 11:51:34 +0530

    Update afk.py

--- MissCutie/modules/afk.py
+++ MissCutie/modules/afk.py
@@ -170,8 +170,10 @@ async def check_afk(update: Update, context: ContextTypes.DEFAULT_TYPE, user_id:
 
 
 __help__ = """
- ➥ `/afk <reason>`*:* mark yourself as AFK (away from keyboard).
- ➥ `brb <reason>`*:* same as the afk command - but not a command.
+*AFK*
+
+➥ `/afk <reason>`*:* mark yourself as AFK (away from keyboard).
+➥ `brb <reason>`*:* same as the afk command - but not a command.
 When marked as AFK, any mentions will be replied to with a message to say you're not available!
 """
 
