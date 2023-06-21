acbb4aab9c09589889bb574061015e1a71fa324a
Saif Ali <sachinsaif03@gmail.com>
2023-06-21 19:20:16 +0530

    Delete youtube.py

--- MissCutie/modules/youtube.py
+++
@@ -1,9 +0,0 @@
-__mod_name__ = "YouTube"
-__help__ = """
-*Music*
-➥ /song to download  any song
-➥ /music to download song
-
-*Video*
-➥ /video to download video song
-➥ /vsong to download video song """
