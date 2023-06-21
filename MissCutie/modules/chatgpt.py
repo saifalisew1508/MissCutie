9241c8ac859ae5485d7baff27ba30d65fb8b3a6c
Saif Ali <sachinsaif03@gmail.com>
2023-06-21 20:30:06 +0530

    bump to PTB=20.3

--- MissCutie/modules/chatgpt.py
+++ MissCutie/modules/chatgpt.py
@@ -1,41 +1,44 @@
-import requests
 import os
-import json
-import random
-import asyncio
-from pyrogram import Client, filters, enums
-from pyrogram.types import Message
-from MissCutie import pbot as ren
-
-CMD_HANDLER = ["!", "/"]  # your change handler
-
-cmd = CMD_HANDLER
-
-OPENAI_API = "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"
+import openai
+import logging
+from telegram.ext import filters, ContextTypes, CommandHandler, MessageHandler
+from telegram import Update
+from datetime import datetime
+from MissCutie import application, OPENAI_API_KEY
 
 
-@ren.on_message(filters.command(["ai", "ask"], cmd) & filters.private | filters.group)
-async def chatgpt(c: Client, m: Message):
-    randydev = (m.text.split(None, 1)[1] if len(m.command) != 1 else None)
-    if not randydev:
-        await m.reply(f"use command <code>/{m.command[0]} [question]</code> to ask questions using the API.")
-        return
-    headers = {"Content-Type": "application/json", "Authorization": f"Bearer {OPENAI_API}"}
 
-    json_data = {
-        "prompt": randydev,
-        "model": "text-davinci-003",
-        "temperature": 0.5,
-        "max_tokens": 1024,
-        "n": 1,
-        "stop": None,
-        "top_p": 0.3,
-        "frequency_penalty": 0.5
-    }
-    try:
-        response = requests.post("https://api.openai.com/v1/completions", headers=headers, json=json_data).json()
-        await c.send_chat_action(m.chat.id, enums.ChatAction.TYPING)
-        await asyncio.sleep(2)
-        await c.send_message(m.chat.id, response["choices"][0]["text"], reply_to_message_id=m.id)
-    except Exception:
-        await c.send_message(m.chat.id, "Sorry, I couldn't retrieve the answer.", reply_to_message_id=m.id)
+
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_message = update.message.text.lower()
+    if user_message in ["hi", "hello", "coé", "oi"]:
+        await update.message.reply_text(f"Hello, how can I help you?")
+    if user_message in ["time?", "time", "hora"]:
+        await update.message.reply_text(f" TIME - {datetime.now()}")
+    # return f"You sent the following: {user_message}"
+
+
+async def gpt(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print(update.message.text)
+    question = str(update.message.text[5:].capitalize())
+    print(f"QUESTION: {question}")
+    response = chatGPT_message(question)
+    await update.message.reply_text(
+        f"Question: {question}.\n"
+        f"Answer: {response['choices'][0]['message']['content']}"
+    )
+
+
+def chatGPT_message(question):
+    openai.api_key = OPENAI_API_KEY
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": f"{question}"},
+        ]
+    )
+    return response
+
+
+application.add_handler(CommandHandler("gpt", gpt))
+application.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), handle_message))
