6182ac2e40fbad06a187b2d2ad178df6aa6c257f
Saif Ali <sachinsaif03@gmail.com>
2023-06-21 13:20:01 +0530

    trying

--- MissCutie/modules/chatgpt.py
+++ MissCutie/modules/chatgpt.py
@@ -1,41 +1,41 @@
-# Copyright (C) 2020-2023 TeamKillerX <https://github.com/TeamKillerX>
-#
-# This file is part of TeamKillerX project,
-# and licensed under GNU Affero General Public License v3.
-# See the GNU Affero General Public License for more details.
-#
-# All rights reserved. See COPYING, AUTHORS.
-#
-
 import requests
 import os
 import json
 import random
 import asyncio
-from pyrogram import *
-from pyrogram.types import *
-from pyrogram.errors import MessageNotModified 
-
+from pyrogram import Client, filters, enums
+from pyrogram.types import Message
 from MissCutie import pbot as ren
-CMD_HANDLER = ["!", "/"] # your change handler
+
+CMD_HANDLER = ["!", "/"]  # your change handler
 
 cmd = CMD_HANDLER
 
 OPENAI_API = "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"
 
+
 @ren.on_message(filters.command(["ai", "ask"], cmd) & filters.private | filters.group)
 async def chatgpt(c: Client, m: Message):
     randydev = (m.text.split(None, 1)[1] if len(m.command) != 1 else None)
     if not randydev:
-       await m.reply(f"use command <code>/{m.command[0]} [question]</code> to ask questions using the API.")
-       return
+        await m.reply(f"use command <code>/{m.command[0]} [question]</code> to ask questions using the API.")
+        return
     headers = {"Content-Type": "application/json", "Authorization": f"Bearer {OPENAI_API}"}
 
-    json_data = {"prompt": randydev, "model": "text-davinci-003", "temperature": 0.5, "max_tokens": 1024, "n": 1, "stop": None, "top_p": 0.3, "frequency_penalty": 0.5}
+    json_data = {
+        "prompt": randydev,
+        "model": "text-davinci-003",
+        "temperature": 0.5,
+        "max_tokens": 1024,
+        "n": 1,
+        "stop": None,
+        "top_p": 0.3,
+        "frequency_penalty": 0.5
+    }
     try:
         response = requests.post("https://api.openai.com/v1/completions", headers=headers, json=json_data).json()
         await c.send_chat_action(m.chat.id, enums.ChatAction.TYPING)
         await asyncio.sleep(2)
-        await c.send_message(m.chat.id, response["choices"][0]["text"], reply_to_message_id=m.id)
+        await c.send_message(m.chat.id, response["choices"][0]["text"], reply_to_message_id=m.message_id)
     except Exception:
-        await c.send_message(m.chat.id, "Yahh, sorry i can't get your answer.", reply_to_message_id=m.id)
+        await c.send_message(m.chat.id, "Sorry, I couldn't retrieve the answer.", reply_to_message_id=m.message_id)
