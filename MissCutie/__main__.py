383c4f55daa0d49350c28edb142af66283faaddd
Saif Ali <sachinsaif03@gmail.com>
2023-06-22 13:32:33 +0530

    reveert

--- MissCutie/__main__.py
+++ MissCutie/__main__.py
@@ -24,7 +24,6 @@ from MissCutie import (
     BOT_NAME,
     BOT_USERNAME,
     BOT_ID,
-    BOT_PIC,
     application,
     StartTime,
     telethn,
@@ -247,7 +246,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
             )
     else:
         await update.effective_message.reply_photo(
-            BOT_PIC,
+            START_IMG,
             caption="i am alive   !\n<b>i didn't slept since​:</b> <code>{}</code>".format(
                 uptime
             ),
