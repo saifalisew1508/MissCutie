e8a520b10617ae04264b2fd8397dba04118771f6
Saif Ali <sachinsaif03@gmail.com>
2023-06-29 15:38:08 +0530

    trying if it work

--- MissCutie/modules/imdb.py
+++ MissCutie/modules/imdb.py
@@ -1,58 +1,76 @@
-import requests
+from imdb import IMDb
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from MissCutie import pbot
+from MissCutie import pbot as app
+
+ia = IMDb()
 
 
-@pbot.on_message(filters.command("imdb"))
+@app.on_message(filters.command("imdb"))
 async def imdb(client, message):
-    if len(message.command) < 2:
-        return await message.reply_text("Give me any movie name.\n   ᴇx. /imdb Avengers")
-    text = (
-        message.text.split(None, 1)[1]
-        if len(message.command) < 3
-        else message.text.split(None, 1)[1].replace(" ", "%20")
-    )
-    url = requests.get(f"https://api.safone.tech/tmdb?query={text}").json()["results"][
-        0
-    ]
-    poster = url["poster"]
-    imdb_link = url["imdbLink"]
-    title = url["title"]
-    rating = url["rating"]
-    releasedate = url["releaseDate"]
-    description = url["overview"]
-    popularity = url["popularity"]
-    runtime = url["runtime"]
-    status = url["status"]
-    await client.send_photo(
-        message.chat.id,
-        poster,
-        caption=f"""**» IMDB Movie Details:**
-‣ **Title** = `{title}`
-‣ **Description** = `{description}`
-‣ **Rating** = `{rating}`
-‣ **Release-Date** = `{releasedate}`
-‣ **Popularity** = `{popularity}`
-‣ **Runtime** = `{runtime}`
-‣ **Status** = `{status}`
-""",
-        reply_markup=InlineKeyboardMarkup(
+    """
+    .imdb <movie name>
+    """
+    if len(message.command) > 1:
+        movie = " ".join(message.command[1:])
+    else:
+        return await message.reply_text("<b>Please enter a movie name</b>")
+    try:
+        movie = ia.get_movie(ia.search_movie(movie)[0].movieID)
+    except:
+        return await message.edit("<b>Movie not found</b>")
+
+    reply_markup = InlineKeyboardMarkup(
+        [
             [
-                [
-                    InlineKeyboardButton(
-                        text="IMDB Link",
-                        url=imdb_link,
-                    ),
-                ],
-            ],
-        ),
+                InlineKeyboardButton(
+                    "More Info",
+                    url=f"https://www.imdb.com/title/tt{movie.movieID}",
+                )
+            ]
+        ]
     )
+    _writers, _directors, _casts = [], [], []
+    try:
+        for _i in movie["writer"]:
+            _writers.append(_i["name"])
+    except:
+        pass
+    try:
+        for _i in movie["director"]:
+            _directors.append(_i["name"])
+    except:
+        pass
+    try:
+        for _i in movie["cast"]:
+            _casts.append(_i["name"])
+        _casts = _casts[:4] if len(_casts) >= 5 else _casts
+    except:
+        pass
+    caption = f"<b>{movie['kind'].capitalize()}</b>\n======\n<b>\
+Title:</b> <code>{movie['title']}</code>\n\
+<b>Year:</b> <code>{movie['year']}</code>\n\
+<b>Rating:</b> <code>{movie['rating'] if 'rating' in movie else 'Not Found'}</code>\n\
+<b>Genre:</b> <code>{', '.join(movie['genres'])}</code>\n\
+<b>Runtime:</b> <code>{movie['runtime'][0] if 'runtime' in movie else 'Not Found'}</code>\n\
+<b>Writers:</b> <code>{', '.join(_writers)}</code>\n\
+<b>Directors:</b> <code>{', '.join(_directors)}</code>\n\
+<b>Actors:</b> <code>{', '.join(_casts)}</code>\n\
+<b>Language:</b> <code>{movie['language'] if 'language' in movie else 'Not Found'}</code>\n\
+<b>Country:</b> <code>{movie['country'] if 'country' in movie else 'Not Found'}</code>\n\
+    "
+    try:
+        m = await message.reply_photo(
+            movie["full-size cover url"],
+            caption=caption,
+            reply_markup=reply_markup,
+        )
+    except KeyError:
+        m = await message.reply_photo(
+            movie["cover url"], caption=caption, reply_markup=reply_markup
+        )
 
-
-__help__ = """
- ➥ /imdb <Movie name>*:* Get full info about a movie from [imdb.com](https://m.imdb.com)
-"""
-
-__mod_name__ = "IMDB"
+    return await m.reply_text(
+        f"<b>Plot:</b> <code>{movie['plot outline'] if 'plot outline' in movie else 'Not available'}</code>"
+    )
