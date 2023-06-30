0c190e2997f5d4d194a2bc1c94970cd24ad182d3
Saif Ali <sachinsaif03@gmail.com>
2023-06-30 17:00:02 +0530

    improve

--- MissCutie/modules/whisper.py
+++ MissCutie/modules/whisper.py
@@ -1,57 +1,54 @@
-
-
 from MissCutie import pbot as app
 from pyrogram import emoji
 from pyrogram.types import (
     InlineQuery,
     InlineQueryResultArticle,
-    InputTextMessageContent, InlineKeyboardMarkup, InlineKeyboardButton,
+    InputTextMessageContent,
+    InlineKeyboardMarkup,
+    InlineKeyboardButton,
 )
 
 lengths = 200
-
 IMG = "https://te.legra.ph/file/5196d5fa658145cb6b9ef.jpg"
 
 @app.on_inline_query()
-async def wishper_ai(_, The: InlineQuery):
-    query = The.query
-    split = query.split(' ', 1)
-    if query == '' or len(query) > lengths \
-            or (query.startswith('@') and len(split) == 1):
-        title = f"🔐 Write a whisper message"
-        content = ("**Send whisper messages through inline mode**\n\n"
-                   "Usage: `@MissCutieRobot [@username] text`")
+async def wishper_ai(_, query: InlineQuery):
+    query_text = query.query
+    split_query = query_text.split(' ', 1)
+
+    if query_text == '' or len(query_text) > lengths or (query_text.startswith('@') and len(split_query) == 1):
+        title = "🔐 Write a whisper message"
+        content = (
+            "**Send whisper messages through inline mode**\n\n"
+            "Usage: `@MissCutieRobot [@username] text`"
+        )
         description = "Usage: @MissCutieRobot [@username] text"
         button = InlineKeyboardButton(
             "More-Help",
             url="https://t.me/MissCutieRobot?start=help"
         )
-
-    elif not query.startswith('@'):
-        title = f"{emoji.EYE} Whisper once to the first one who open it"
-        content = (
-            f"{emoji.EYE} The first one who open the whisper can read it"
-        )
-        description = f"{emoji.SHUSHING_FACE} {query}"
+    elif not query_text.startswith('@'):
+        title = f"{emoji.EYE} Whisper once to the first one who opens it"
+        content = f"{emoji.EYE} The first one who opens the whisper can read it"
+        description = f"{emoji.SHUSHING_FACE} {query_text}"
         button = InlineKeyboardButton(
-            f"🎯 show message",
+            f"🎯 Show message",
             callback_data="show_whisper"
         )
-
     else:
-        u_target = 'anyone' if (x := split[0]) == '@' else x
+        u_target = 'anyone' if (x := split_query[0]) == '@' else x
         title = f"🔒 A whisper message to {u_target}, Only he/she can open it."
         content = f"🔒 A whisper message to {u_target}, Only he/she can open it."
-        description = f"{emoji.SHUSHING_FACE} {split[1]}"
+        description = f"{emoji.SHUSHING_FACE} {split_query[1]}"
         button = InlineKeyboardButton(
-            f"{emoji.LOCKED_WITH_KEY} show message",
+            f"{emoji.LOCKED_WITH_KEY} Show message",
             callback_data="show_whisper"
         )
 
     switch_pm_text = f"{emoji.INFORMATION} Learn how to send whispers"
     switch_pm_parameter = "learn"
-    
-    await The.answer(
+
+    await query.answer(
         results=[
             InlineQueryResultArticle(
                 title=title,
