a19d5441da9364bb0f23143e42c73d4262f65a8e
Saif Ali <sachinsaif03@gmail.com>
2023-06-30 17:06:04 +0530

    Update figlet.py

--- MissCutie/modules/figlet.py
+++ MissCutie/modules/figlet.py
@@ -6,24 +6,38 @@ from MissCutie.events import register
 async def figlet(event):
     if event.fwd_from:
         return
-    CMD_FIG = {"slant": "slant", "3D": "3-d", "5line": "5lineoblique", "alpha": "alphabet", "banner": "banner3-D", "doh": "doh", "iso": "isometric1", "letter": "letters", "allig": "alligator", "dotm": "dotmatrix", "bubble": "bubble", "bulb": "bulbhead", "digi": "digital"}
+
+    CMD_FIG = {
+        "slant": "slant",
+        "3D": "3-d",
+        "5line": "5lineoblique",
+        "alpha": "alphabet",
+        "banner": "banner3-D",
+        "doh": "doh",
+        "iso": "isometric1",
+        "letter": "letters",
+        "allig": "alligator",
+        "dotm": "dotmatrix",
+        "bubble": "bubble",
+        "bulb": "bulbhead",
+        "digi": "digital",
+    }
+
     input_str = event.pattern_match.group(1)
-    if "|" in input_str:
-        text, cmd = input_str.split("|", maxsplit=1)
-    elif input_str is not None:
-        cmd = None
-        text = input_str
-    else:
+    if input_str is None:
         await event.edit("Please add some text to figlet")
         return
-    if cmd is not None:
-        try:
-            font = CMD_FIG[cmd]
-        except KeyError:
+
+    if "|" in input_str:
+        text, cmd = input_str.split("|", maxsplit=1)
+        font = CMD_FIG.get(cmd)
+        if font is None:
             await event.edit("Invalid selected font.")
             return
-        result = pyfiglet.figlet_format(text, font=font)
     else:
-        result = pyfiglet.figlet_format(text)
+        text = input_str
+        font = None
+
+    result = pyfiglet.figlet_format(text, font=font)
     await event.respond("‌‌‎`{}`".format(result))
     await event.delete()
