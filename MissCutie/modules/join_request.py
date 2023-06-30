232de03756992eb29d8550a883a8033f2df78a81
Saif Ali <sachinsaif03@gmail.com>
2023-06-30 17:17:37 +0530

    v3.0

--- MissCutie/modules/join_request.py
+++ MissCutie/modules/join_request.py
@@ -13,17 +13,15 @@ from telegram.constants import ParseMode
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.helpers import mention_html
 
-
 from MissCutie import application
 from MissCutie.modules.helper_funcs.chat_status import check_admin
 from MissCutie.modules.log_channel import loggable
-from MissCutie.modules.sql.join_request import enable_join_req, disable_join_req, join_req_status, migrate_chat
-
+from MissCutie.modules.sql.join_request import enable_join_req, disable_join_req, join_req_status, set_auto_approve, migrate_chat
 
 
 @check_admin(permission="can_invite_users", is_both=True)
 @loggable
-async def set_requests(update: Update,context: ContextTypes.DEFAULT_TYPE):
+async def set_requests(update: Update, context: ContextTypes.DEFAULT_TYPE):
     message = update.effective_message
     chat = update.effective_chat
     args = context.args
@@ -67,30 +65,86 @@ async def set_requests(update: Update,context: ContextTypes.DEFAULT_TYPE):
     return
 
 
+@check_admin(permission="can_invite_users", is_both=True)
+@loggable
+async def set_auto_approve(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    message = update.effective_message
+    chat = update.effective_chat
+    args = context.args
+    user = update.effective_user
+
+    if len(args) > 0:
+        s = args[0].lower()
+
+        if s in ["yes", "on", "true"]:
+            set_auto_approve(chat.id, True)
+            await message.reply_html(
+                "Enabled auto-approve for join requests in {}".format(html.escape(chat.title)))
+            log_message = (
+                f"#AUTOAPPROVE\n"
+                f"Enabled\n"
+                f"<b>Admin:</b> {mention_html(user.id, user.first_name)}"
+            )
+            return log_message
+
+        elif s in ["off", "no", "false"]:
+            set_auto_approve(chat.id, False)
+            await message.reply_html(
+                "Disabled auto-approve for join requests in {}".format(html.escape(chat.title)))
+            log_message = (
+                f"#AUTOAPPROVE\n"
+                f"Disabled\n"
+                f"<b>Admin:</b> {mention_html(user.id, user.first_name)}"
+            )
+            return log_message
+
+        else:
+            await message.reply_text("Unrecognized arguments {}".format(s))
+            return
+
+    await message.reply_html(
+        "Auto-approve for join requests is currently <b><i>{}</i></b> in <code>{}</code>\n\n"
+        "When this setting is on, new join requests will be automatically approved.".format(
+            join_req_status(chat.id), html.escape(chat.title)))
+    return
+
 
 async def chat_join_req(update: Update, context: ContextTypes.DEFAULT_TYPE):
     bot = context.bot
     user = update.chat_join_request.from_user
     chat = update.chat_join_request.chat
     keyboard = InlineKeyboardMarkup(
+        [
             [
-                [
-                    InlineKeyboardButton(
-                            "✅ Approve", callback_data="cb_approve={}".format(user.id)
-                    ),
-                    InlineKeyboardButton(
-                            "❌ Decline", callback_data="cb_decline={}".format(user.id)
-                    ),
-                ]
+                InlineKeyboardButton(
+                    "✅ Approve", callback_data="cb_approve={}".format(user.id)
+                ),
+                InlineKeyboardButton(
+                    "❌ Decline", callback_data="cb_decline={}".format(user.id)
+                ),
             ]
+        ]
     )
-    await bot.send_message(
+
+    # Check if auto-approve is enabled
+    if join_req_status(chat.id) and join_req_status(chat.id):
+        await bot.approve_chat_join_request(chat.id, user.id)
+        await bot.send_message(
             chat.id,
-            "{} wants to join {}".format(
-                    mention_html(user.id, user.first_name), chat.title or "this chat"
+            "{} has been automatically approved and joined {}".format(
+                mention_html(user.id, user.first_name), chat.title or "this chat"
             ),
-            reply_markup=keyboard,
             parse_mode=ParseMode.HTML,
+        )
+        return
+
+    await bot.send_message(
+        chat.id,
+        "{} wants to join {}".format(
+            mention_html(user.id, user.first_name), chat.title or "this chat"
+        ),
+        reply_markup=keyboard,
+        parse_mode=ParseMode.HTML,
     )
 
 
@@ -110,8 +164,8 @@ async def approve_joinReq(update: Update, context: ContextTypes.DEFAULT_TYPE) ->
         joined_mention = mention_html(user_id, html.escape(joined_user.user.first_name))
         admin_mention = mention_html(user.id, html.escape(user.first_name))
         await update.effective_message.edit_text(
-                f"{joined_mention}'s join request was approved by {admin_mention}.",
-                parse_mode="HTML",
+            f"{joined_mention}'s join request was approved by {admin_mention}.",
+            parse_mode="HTML",
         )
         logmsg = (
             f"<b>{html.escape(chat.title)}:</b>\n"
@@ -142,8 +196,8 @@ async def decline_joinReq(update: Update, context: ContextTypes.DEFAULT_TYPE) ->
         joined_mention = mention_html(user_id, html.escape(joined_user.user.first_name))
         admin_mention = mention_html(user.id, html.escape(user.first_name))
         await update.effective_message.edit_text(
-                f"{joined_mention}'s join request was declined by {admin_mention}.",
-                parse_mode="HTML",
+            f"{joined_mention}'s join request was declined by {admin_mention}.",
+            parse_mode="HTML",
         )
         logmsg = (
             f"<b>{html.escape(chat.title)}:</b>\n"
@@ -156,15 +210,14 @@ async def decline_joinReq(update: Update, context: ContextTypes.DEFAULT_TYPE) ->
     except Exception as e:
         await update.effective_message.edit_text(str(e))
         pass
-        
-        
+
+
 def __migrate__(old_chat_id, new_chat_id):
     migrate_chat(old_chat_id, new_chat_id)
-    
-    
 
 
 application.add_handler(CommandHandler('joinrequest', set_requests))
+application.add_handler(CommandHandler('autoapprove', set_auto_approve))  # Added handler for the V3.0 new command
 application.add_handler(ChatJoinRequestHandler(callback=chat_join_req, block=False))
 application.add_handler(CallbackQueryHandler(callback=approve_joinReq, pattern=r"cb_approve="))
 application.add_handler(CallbackQueryHandler(callback=decline_joinReq, pattern=r"cb_decline="))
