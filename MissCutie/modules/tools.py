88b2255b832221176d92b3d4d4c1327d17156014
Saif Ali <sachinsaif03@gmail.com>
2023-07-02 19:03:01 +0530

    Add Some Semeks

--- MissCutie/modules/tools.py
+++ MissCutie/modules/tools.py
@@ -1,6 +1,56 @@
 __help__ = """
-*Make Carbon*
-/carbon 
+*Carbon*
+/carbon *:* Make Carbon To Given or Replied Text
+
+*Couples*
+➥ /couples *:* Choose 2 users and send their name as couples in your chat.
+
+*Encryption & Decryption*
+➥ /encrypt *:* Encrypts The Given Text
+➥ /decrypt *:* Decrypts Previously Ecrypted Text
+ 
+*GitHub*
+➥ /github <username>*:* get information about a github user.
+
+*GPS & Location*
+➥ /gps <location>*:* Get gps location.
+
+*Mamefy*
+➥ /mmf <TEXT>*:* Add Text to Replied Sticker
+
+*Prank*
+➥ /horny *:* check your current hornyess
+➥ /gay *:* check your current guyness
+➥ /lezbian *:* check ur current lazbian
+➥ /boob *:* check your current boobs size
+➥ /cute *:* check your current cuteness
+
+*Paste*
+➥ /paste *:* Saves replied content to nekobin.com and replies with a url
+ 
+*PDF Tool*
+/pdf *:* Make PDF of Replied Image
+
+*QR Code*
+➥ /getqr *:* Get QR Code content from a replied photo.
+➥ /makeqr <text> *:* Make a QR Code containing the given text.
+
+*WallPaper*
+➥ /wall <query>*:* get a wallpaper from wall.alphacoders.com
+
+*Weather*
+➥ /weather <city>*:* Advanced weather module, usage same as /weather
+➥ /weather moon*:* Get the current status of moon
+
+*Website Screenshot*
+➥ /webss <URL Link>*;* Sends the screenshot of the given url.
+
+*Write Tool*
+➥ /write <text>*:* Writes the given text.
+
+*Zip*
+➥ /zip *:* reply to a telegram file to compress it in .zip format
+➥ /unzip *:* reply to a telegram file to decompress it from the .zip format
 """
 
 __mod_name__ = "Tools"
