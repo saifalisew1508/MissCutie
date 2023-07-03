71225582210ef06d42873bd0fa7f11779aad35ff
Saif Ali <sachinsaif03@gmail.com>
2023-07-03 16:48:34 +0530

    merge tiktok dl

--- MissCutie/modules/igdl.py
+++ MissCutie/modules/igdl.py
@@ -32,3 +32,32 @@ async def igdl(bot, message):
           return await ig.edit("The URL you provided is neither reel, nor igtv nor post, therefore i can't process it. If you are right please report this at @PublicSource_Chat.")
   else:
       await ig.edit("Parameter limit exceeded, Read Help Menu to know how command works.")
+
+
+
+
+
+@pbot.on_message(filters.command("ttdl"))
+async def ttdl(bot, message):
+  
+  tt = await message.reply("`Processing...`")
+  
+  if len(message.command) == 1:
+      return await tt.edit("No URL provided to download, Read Help Menu to know how command works")
+  
+  elif len(message.command) == 2:
+      url = message.command[1]
+      out = requests.get(f"https://open-apis-rest.up.railway.app/api/tiktok?url={url}").json()
+      try:
+          m = out["error"]
+          return await ig.edit("Wrong URL provided.")
+      except:
+          pass
+      try:
+          video = out["data"]["server1"]["video"]
+      except:
+          return await tt.edit("API didn't responded, please report it at @PublicSource_Chat or try again later.")
+      await message.reply_video(video=video, caption="Here is your video.")
+      return await tt.delete()
+  else:
+      await tt.edit("Parameter limit exceeded, Read Help Menu to know how command works.")
