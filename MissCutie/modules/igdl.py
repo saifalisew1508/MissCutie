043c280097142bc5234eb0e7d53b7eba7e5fab8e
Saif Ali <sachinsaif03@gmail.com>
2023-07-03 16:44:09 +0530

    `New Download Method`

--- MissCutie/modules/igdl.py
+++ MissCutie/modules/igdl.py
@@ -1,91 +1,34 @@
-"""
-import asyncio
-import instaloader
 import requests
 from pyrogram import filters
-import os
-from MissCutie import pbot as app
-from MissCutie import pbot as client
+from MissCutie import pbot
 
-
-
-L = instaloader.Instaloader()
-L.login("im_broken2023", "Saif@1508")
-
-
-
-async def download_instagram_post(post_code, target_folder):
-    post = instaloader.Post.from_shortcode(L.context, post_code)
-    await L.download_post(post, target=target_folder)
-
-async def download_instagram_story(username, target_folder):
-    profile = instaloader.Profile.from_username(L.context, username)
-    stories = profile.get_stories()
-    for story in stories:
-        await L.download_storyitem(story, target=target_folder)
-
-async def download_instagram_igtv(igtv_code, target_folder):
-    igtv = instaloader.IGTV.from_shortcode(L.context, igtv_code)
-    await L.download_igtv([igtv], target=target_folder)
-
-async def download_instagram_reel(reel_code, target_folder):
-    reel = instaloader.Post.from_shortcode(L.context, reel_code)
-    await L.download_reels([reel], target=target_folder)
-
-@app.on_message(filters.command("ig", prefixes="/"))
-async def save_instagram_content(client, message):
-    url = message.text.split(" ")[1]
-    try:
-        if "/p/" in url:
-            # If it's a post
-            post_code = url.split("/p/")[1].split("/")[0]
-            media_path = f"saved_posts/{post_code}.jpg"
-            await download_instagram_post(post_code, "saved_posts")
-            caption = "Instagram Post:"
-            is_photo = True
-        elif "/stories/" in url:
-            # If it's a story
-            username = url.split("/stories/")[1].split("/")[0]
-            media_path = f"saved_stories/{username}"
-            await download_instagram_story(username, "saved_stories")
-            caption = "Instagram Story:"
-            is_photo = True
-        elif "/reel/" in url:
-            # If it's a reel
-            reel_code = url.split("/reel/")[1].split("/")[0]
-            media_path = f"saved_reels/{reel_code}.mp4"
-            await download_instagram_reel(reel_code, "saved_reels")
-            caption = "Instagram Reel:"
-            is_photo = False
-        elif "/tv/" in url:
-            # If it's an IGTV video
-            igtv_code = url.split("/tv/")[1].split("/")[0]
-            media_path = f"saved_igtv/{igtv_code}.mp4"
-            await download_instagram_igtv(igtv_code, "saved_igtv")
-            caption = "Instagram IGTV:"
-            is_photo = False
-        else:
-            await client.send_message(message.chat.id, "Invalid URL! Please provide a valid Instagram post, reel, IGTV, or story URL.")
-            return
-
-        # Send the media as a Telegram message
-        await client.send_chat_action(message.chat.id, "upload_photo" if is_photo else "upload_video")
-        if is_photo:
-            await client.send_photo(
-                chat_id=message.chat.id,
-                photo=media_path,
-                caption=caption
-            )
-        else:
-            await client.send_video(
-                chat_id=message.chat.id,
-                video=media_path,
-                caption=caption
-            )
-        os.remove(media_path)  # Remove the downloaded media file
-
-    except Exception as e:
-        await client.send_message(message.chat.id, f"An error occurred while processing the content: {str(e)}")
-
-
-"""
+@pbot.on_message(filters.command("igdl"))
+async def igdl(bot, message):
+  
+  ig = await message.reply("`Processing...`")
+  
+  if len(message.command) == 1:
+      return await ig.edit("No URL provided to download, Read Help Menu to know how command works")
+  
+  elif len(message.command) == 2:
+      tk = message.text.split("/")
+      pk = message.text.split()
+      nk = pk[1]
+      uk = nk.split("/")
+      url = uk[0] + "//" + uk[2] + "/" + uk[3] + "/" + uk[4]
+      if tk[4] == "reel":
+          out = requests.get(f"https://open-apis-rest.up.railway.app/api/instareel?url={url}").json()
+          try:
+              reel = out["data"]["data"]["url"]
+          except:
+              return await ig.edit("API didn't responded, please report it at @HagadmansaChat or try again later.")
+          await message.reply_video(video=reel, caption="Here is your reel.")
+          return await ig.delete()
+      elif tk[4] == "p":
+          return await ig.edit("Photos Downloader will be added soon, Currently only Reels Downloader available.")
+      elif tk[4] == "tv":
+          return await ig.edit("IGTV Downloader will be added soon, Currently only Reels Downloader available.")
+      else:
+          return await ig.edit("The URL you provided is neither reel, nor igtv nor post, therefore i can't process it. If you are right please report this at @PublicSource_Chat.")
+  else:
+      await ig.edit("Parameter limit exceeded, Read Help Menu to know how command works.")
