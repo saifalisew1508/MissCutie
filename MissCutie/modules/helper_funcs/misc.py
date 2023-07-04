0522fa49051473f8213212c21631c75de7c352e7
Saif Ali <sachinsaif03@gmail.com>
2023-07-04 13:42:53 +0530

    Fuck it

--- MissCutie/modules/helper_funcs/misc.py
+++ MissCutie/modules/helper_funcs/misc.py
@@ -82,19 +82,28 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
         pairs = pairs[modulo_page * 10 : 10 * (modulo_page + 1)] + [
             (
                 EqInlineKeyboardButton(
-                    "◁", callback_data="{}_prev({})".format(prefix, modulo_page)
-                ),
-                EqInlineKeyboardButton(
-                    "Home", callback_data="saif_back"
-                ),
-                EqInlineKeyboardButton(
-                    "▷", callback_data="{}_next({})".format(prefix, modulo_page)
+                    "🔙", callback_data="saif_back"
                 ),
             )
         ]
 
+        
+#        pairs = pairs[modulo_page * 10 : 10 * (modulo_page + 1)] + [
+#            (
+#                EqInlineKeyboardButton(
+#                    "◁", callback_data="{}_prev({})".format(prefix, modulo_page)
+#                ),
+#                EqInlineKeyboardButton(
+#                    "Home", callback_data="saif_back"
+#                ),
+#                EqInlineKeyboardButton(
+#                    "▷", callback_data="{}_next({})".format(prefix, modulo_page)
+#                ),
+#            )
+#        ]
+
     else:
-        pairs += [[EqInlineKeyboardButton("Home", callback_data="saif_back")]]
+        pairs += [[EqInlineKeyboardButton("🔙", callback_data="saif_back")]]
 
     return pairs
 
