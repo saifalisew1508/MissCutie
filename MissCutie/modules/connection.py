cddbdd998985e05cb8a0e1c0ac5ddd8cef4affd4
Saif Ali <sachinsaif03@gmail.com>
2023-07-06 16:12:52 +0530

    Update connection.py

--- MissCutie/modules/connection.py
+++ MissCutie/modules/connection.py
@@ -411,18 +411,11 @@ async def connect_button(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 __mod_name__ = "Connection"
 
-__help__ = """
-Sometimes, you just want to add some notes and filters to a group chat, but you don't want everyone to see; This is where connections come in...
-This allows you to connect to a chat's database, and add things to it without the commands appearing in chat! For obvious reasons, you need to be an admin to add things; but any member in the group can view your data.
 
- ➥ /connect: Connects to chat (Can be done in a group by /connect or /connect <chat id> in PM)
- ➥ /connection: List connected chats
- ➥ /disconnect: Disconnect from a chat
- ➥ /helpconnect: List available commands that can be used remotely
+from MissCutie.modules.language import gs
 
-*Admin only:*
- ➥ /allowconnect <yes/no>: allow a user to connect to a chat
-"""
+def get_help(chat):
+    return gs(chat, "connections_help")
 
 CONNECT_CHAT_HANDLER = CommandHandler("connect", connect_chat, block=False)
 CONNECTION_CHAT_HANDLER = CommandHandler("connection", connection_chat, block=False)
