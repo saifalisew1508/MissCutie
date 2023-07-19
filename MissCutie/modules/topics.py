fdf14059782981afa3c95f9b7bf95322ac43bed5
Saif Ali <sachinsaif03@gmail.com>
2023-07-19 19:52:34 +0530

    Update topics.py

--- MissCutie/modules/topics.py
+++ MissCutie/modules/topics.py
@@ -198,21 +198,6 @@ async def open_topic(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 __mod_name__ = "Topics"
 
-__help__ = """
-Telegram introuduced new way of managing your chat called Forums(Topics)
-
-As a group management bot I have some useful commands to help you
-create, delete, close and reopen topics in your chat.
- ➥ `/topicnew`*:* Create new topic, requires topic name to create.
- ➥ `/topicdel`*:* Delete an existing topic, requires topic ID to delete.  
- ➥ `/topicclose`*:* Close an existing topic, requires topic ID to close.
- ➥ `/topicopen`*:* Open an already closed topic, requires topic ID to open.
- ➥ `/setactiontopic`*:* Set issuing topic for action messages such as welcome, goodbye, warns, bans,..etc
- ➥ `/delactiontopic`*:* Delete default topic for actions messages.
-"""
-
-
-
 
 SET_TOPIC_HANDLER = CommandHandler("setactiontopic", set_topic_action, block=False)
 DEL_TOPIC_HANDLER = CommandHandler("delactiontopic", del_topic_action, block=False)
