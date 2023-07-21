d4339ec7b59c5c43b2e1166207ec3a89a2e113ae
Saif Ali <sachinsaif03@gmail.com>
2023-07-21 16:44:39 +0530

    Update whois.py

--- MissCutie/modules/whois.py
+++ MissCutie/modules/whois.py
@@ -1,87 +1,70 @@
+import os
+import time
 from datetime import datetime
-
-from pyrogram import filters
-from pyrogram.errors import PeerIdInvalid
-from pyrogram.types import Message, User
-
+from pyrogram import Client, filters, enums
+from pyrogram.errors import UserNotParticipant
+from info import COMMAND_HAND_LER
+from MissCutie.utils.extract_user import extract_user
+from MissCutie.utils.last_online_help import last_online
 from MissCutie import pbot
 
 
-def ReplyCheck(message: Message):
-    reply_id = None
-
-    if message.reply_to_message:
-        reply_id = message.reply_to_message.message_id
-
-    elif not message.from_user.is_self:
-        reply_id = message.message_id
-
-    return reply_id
-
-
-infotext = (
-    "[{full_name}](tg://user?id={user_id})\n\n"
-    " ➻ user id: `{user_id}`\n"
-    " ➻ first name: `{first_name}`\n"
-    " ➻ last name: `{last_name}`\n"
-    " ➻ username: `{username}`\n"
-    " ➻ last seen: `{last_online}`"
-)
-
-
-def LastOnline(user: User):
-    if user.is_bot:
-        return ""
-    elif user.status == "recently":
-        return "recently"
-    elif user.status == "within_week":
-        return "within the last week"
-    elif user.status == "within_month":
-        return "within the last month"
-    elif user.status == "long_time_ago":
-        return "a long time ago :("
-    elif user.status == "online":
-        return "currently online"
-    elif user.status == "offline":
-        return datetime.fromtimestamp(user.status.date).strftime(
-            "%a, %d %b %Y, %H:%M:%S"
-        )
-
-
-def FullName(user: User):
-    return user.first_name + " " + user.last_name if user.last_name else user.first_name
-
-
-@pbot.on_message(filters.command("whois"))
-async def whois(client, message):
-    cmd = message.command
-    if not message.reply_to_message and len(cmd) == 1:
-        get_user = message.from_user.id
-    elif len(cmd) == 1:
-        get_user = message.reply_to_message.from_user.id
-    elif len(cmd) > 1:
-        get_user = cmd[1]
-        try:
-            get_user = int(cmd[1])
-        except ValueError:
-            pass
+@pbot.on_message(filters.command(["whois", "info"])
+async def who_is(client, message):
+    status_message = await message.reply_text("Wait Bro, Let Me Check 🙂")
+    
+    from_user = None
+    from_user_id, _ = extract_user(message)
     try:
-        user = await client.get_users(get_user)
-    except PeerIdInvalid:
-        await message.reply("I don't know that user.")
+        from_user = await client.get_users(from_user_id)
+    except Exception as error:
+        await status_message.edit(str(error))
         return
-    desc = await client.get_chat(get_user)
-    desc = desc.description
-    await message.reply_text(
-        infotext.format(
-            full_name=FullName(user),
-            user_id=user.id,
-            user_dc=user.dc_id,
-            first_name=user.first_name,
-            last_name=user.last_name if user.last_name else "",
-            username=user.username if user.username else "",
-            last_online=LastOnline(user),
-            bio=desc if desc else "`empty.`",
-        ),
-        disable_web_page_preview=True,
+    
+    if from_user is None:
+        await status_message.edit("No valid user_id / message specified")
+        return
+    
+    first_name = from_user.first_name or ""
+    last_name = from_user.last_name or ""
+    username = from_user.username or ""
+    
+    message_out_str = (
+        "<b>᚛› 𝙽𝙰𝙼𝙴 :</b> "
+        f"<a href='tg://user?id={from_user.id}'>{first_name}</a>\n"
+        f"<b>᚛› 𝚂𝚄𝙵𝙵𝙸𝚇 :</b> {last_name}\n"
+        f"<b>᚛› 𝚄𝚂𝙴𝚁𝙽𝙰𝙼𝙴 :</b> @{username}\n"
+        f"<b>᚛› 𝚄𝚂𝙴𝚁 𝙸𝙳 :</b> <code>{from_user.id}</code>\n"
+        f"<b>᚛› 𝚄𝚂𝙴𝚁 𝙻𝙸𝙽𝙺 :</b> {from_user.mention}\n" if from_user.username else ""
+        f"<b>᚛› 𝙸𝚂 𝙰𝙲𝙲𝙾𝚄𝙽𝚃 𝙳𝙴𝙻𝙴𝚃𝙴𝙳 :</b> True\n" if from_user.is_deleted else ""
+        f"<b>᚛› 𝙸𝚂 𝚅𝙴𝚁𝙸𝙵𝙸𝙴𝙳 :</b> True" if from_user.is_verified else ""
+        f"<b>᚛› 𝙸𝚂 𝚂𝙲𝙰𝙼 :</b> True" if from_user.is_scam else ""
+        f"<b>᚛› 𝙻𝙰𝚂𝚃 𝚂𝙴𝙴𝙽 :</b> <code>{last_online(from_user)}</code>\n\n"
     )
+    
+    if message.chat.type in [enums.ChatType.SUPERGROUP, enums.ChatType.CHANNEL]:
+        try:
+            chat_member_p = await message.chat.get_member(from_user.id)
+            joined_date = datetime.fromtimestamp(chat_member_p.joined_date or time.time()).strftime("%Y.%m.%d %H:%M:%S")
+            message_out_str += "<b>Joined on:</b> <code>" f"{joined_date}" "</code>\n"
+        except UserNotParticipant:
+            pass
+    
+    chat_photo = from_user.photo
+    if chat_photo:
+        local_user_photo = await client.download_media(message=chat_photo.big_file_id)
+        await message.reply_photo(
+            photo=local_user_photo,
+            quote=True,
+            caption=message_out_str,
+            disable_notification=True
+        )
+        os.remove(local_user_photo)
+    else:
+        await message.reply_text(
+            text=message_out_str,
+            quote=True,
+            disable_notification=True
+        )
+    
+    await status_message.delete()
