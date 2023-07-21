d52cd053493d465109be808aeb8a7897e552a252
Saif Ali <sachinsaif03@gmail.com>
2023-07-21 16:47:52 +0530

    Update json.py

--- MissCutie/modules/json.py
+++ MissCutie/modules/json.py
@@ -1,54 +1,47 @@
-import io
+import os
+from pyrogram import Client, filters, enums
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message, CallbackQuery
+from MissCutie import pbot
 
-from telethon import types
-from telethon.tl import functions
-
-from MissCutie import telethn as tbot
-from MissCutie.events import register
-
-
-async def is_register_admin(chat, user):
-    if isinstance(chat, (types.InputPeerChannel, types.InputChannel)):
-        return isinstance(
-            (
-                await tbot(functions.channels.GetParticipantRequest(chat, user))
-            ).participant,
-            (types.ChannelParticipantAdmin, types.ChannelParticipantCreator),
-        )
-    if isinstance(chat, types.InputPeerUser):
-        return True
-
-
-@register(pattern="^/json$")
-async def _(event):
-    if event.fwd_from:
-        return
-    if event.is_group:
-        if not (await is_register_admin(event.input_chat, event.message.sender_id)):
-            await event.reply(
-                "Need admin to use this in groups﹐ but you can use it in my PM."
-            )
-            return
-
+@pbot.on_message(filters.command(["json", 'js', 'showjson']))
+async def jsonify(_, message):
     the_real_message = None
     reply_to_id = None
-    if event.reply_to_msg_id:
-        previous_message = await event.get_reply_message()
-        the_real_message = previous_message.stringify()
-        reply_to_id = event.reply_to_msg_id
+
+    if message.reply_to_message:
+        the_real_message = message.reply_to_message
     else:
-        the_real_message = event.stringify()
-        reply_to_id = event.message.id
-    if len(the_real_message) > 4095:
-        with io.BytesIO(str.encode(the_real_message)) as out_file:
-            out_file.name = "json.text"
-            await tbot.send_file(
-                event.chat_id,
-                out_file,
-                force_document=True,
-                allow_cache=False,
-                reply_to=reply_to_id,
-            )
-            await event.delete()
-    else:
-        await event.reply("`{}`".format(the_real_message))
+        the_real_message = message
+    try:
+        pk = InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        text="𝙲𝙻𝙾𝚂𝙴",
+                        callback_data="close_data"
+                    )
+                ]
+            ]
+        )
+        await message.reply_text(f"<code>{the_real_message}</code>", reply_markup=pk, quote=True)
+    except Exception as e:
+        with open("json.text", "w+", encoding="utf8") as out_file:
+            out_file.write(str(the_real_message))
+        reply_markup = InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        text="𝙲𝙻𝙾𝚂𝙴",
+                        callback_data="close_data"
+                    )
+                ]
+            ]
+        )
+        await message.reply_document(
+            document="json.text",
+            caption=str(e),
+            disable_notification=True,
+            quote=True,
+            reply_markup=reply_markup
+        )            
+        os.remove("json.text")
