9d083257835ff86fbf6938c74e6b70da44004843
Saif Ali <sachinsaif03@gmail.com>
2023-08-11 13:51:25 +0530

    Update config.py

--- MissCutie/config.py
+++ MissCutie/config.py
@@ -18,9 +18,9 @@ class Config(object):
     API_ID = 23738177  # integer value, dont use ""
     API_HASH = "d54a6609d5cc5ba0094ee206791e1490"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 1930139488  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "PrinceXofficial"
-    SUPPORT_CHAT = "MissCutie_Support"  # Your own group for support, do not add the @
+    OWNER_ID = 5982904247  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "LostedPerson"
+    SUPPORT_CHAT = "PublicSource_Chat"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
         -1001660668029
     )  # Prints any new group the bot is added to, prints just the name and ID.
