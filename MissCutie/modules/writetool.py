493dc323caa01cacbac86b6608293ca6a69196fd
Saif Ali <sachinsaif03@gmail.com>
2023-11-17 05:13:13 +0530

    Update writetool.py

--- MissCutie/modules/writetool.py
+++ MissCutie/modules/writetool.py
@@ -1,56 +1,40 @@
 import requests
-from pyrogram import filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton
+from telegram.constants import ParseMode
+from telegram.ext import ContextTypes, CommandHandler, filters, CallbackQueryHandler
+from telegram.helpers import mention_html
 
-from MissCutie import BOT_NAME, BOT_USERNAME
-from MissCutie import pyroclient as saif
+from MissCutie import BOT_NAME, BOT_USERNAME, application
 
 
-@saif.on_message(filters.command("write"))
-async def handwrite(_, message: Message):
+async def handwrite(update, context: CallbackContext):
+    message = update.effective_message
+
     if not message.reply_to_message:
         text = message.text.split(None, 1)[1]
-        m = await saif.send_message(
-            message.chat.id, "`Please wait...,\n\nWriting your text...`"
-        )
-        API = f"https://api.sdbots.tk/write?text={text}"
-        req = requests.get(API).url
-        caption = f"""
-sucessfully written text 💘
-
-✨ **Written By :** [{BOT_NAME}](https://t.me/{BOT_USERNAME})
-🥀 **Requested By :** {message.from_user.mention}
-🔗 **Link :** `{req}`
-"""
-        await m.delete()
-        await saif.send_photo(
-            message.chat.id,
-            photo=req,
-            caption=caption,
-            reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton("• telegraph •", url=f"{req}")]]
-            ),
-        )
     else:
-        lol = message.reply_to_message.text
-        m = await saif.send_message(
-            message.chat.id, "`Please wait...,\n\nWriting your text...`"
-        )
-        API = f"https://api.sdbots.tk/write?text={lol}"
-        req = requests.get(API).url
-        caption = f"""
-sucessfully written text 💘
+        text = message.reply_to_message.text
+
+    m = await update.effective_message.reply_text("Please wait writing your text to notebook...")
+    API = f"https://notes.apinepdev.workers.dev/?text={text}"
+    NOTE = requests.get(API).url
+
+    caption = f"""
+Successfully written text 💘
 
 ✨ **Written By :** [{BOT_NAME}](https://t.me/{BOT_USERNAME})
-🥀 **Requested By :** {message.from_user.mention}
-🔗 **Link :** `{req}`
+🥀 **Requested By :** {mention_html(message.from_user.id, message.from_user.first_name)}
+🔗 **Link :** `{NOTE}`
 """
-        await m.delete()
-        await saif.send_photo(
-            message.chat.id,
-            photo=req,
-            caption=caption,
-            reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton("Telegraph", url=f"{req}")]]
-            ),
-        )
+
+    await m.delete()
+    await update.effective_message.reply_photo(
+        photo=NOTE,
+        caption=caption,
+        reply_markup=InlineKeyboardMarkup(
+            [[InlineKeyboardButton("• Telegraph •", url=NOTE)]]
+        ),
+        parse_mode=ParseMode.MARKDOWN,
+    )
+
+application.add_handler(CommandHandler("write", handwrite))
