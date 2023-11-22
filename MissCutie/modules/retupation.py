f9831a7939624ec28dcb672ba1c894e79f969c9d
Saif Ali <sachinsaif03@gmail.com>
2023-11-22 16:36:29 +0530

    ~Update

--- MissCutie/modules/retupation.py
+++ MissCutie/modules/retupation.py
@@ -10,7 +10,7 @@ import MissCutie.modules.sql.reputation_settings_sql as settings
 
 
 
-async def increase(update: Update, context: CallbackContext):
+async def increase(update: Update, context: ContextTypes.DEFAULT_TYPE):
     msg = update.effective_message
     user1 = update.effective_user
     chat = update.effective_chat
@@ -19,24 +19,24 @@ async def increase(update: Update, context: CallbackContext):
     if msg.reply_to_message:
         user2 = msg.reply_to_message.from_user
         if not settings.user_should_reputate(user1.id):
-            msg.reply_text("You have opted out of reputations, so you are not able to change others reputation neither.")
+            await msg.reply_text("You have opted out of reputations, so you are not able to change others reputation neither.")
             return
         if not settings.user_should_reputate(user2.id):
-            msg.reply_text(f"{user2.full_name} opted out of reputations,"
-                           f" so you are not able to change their reputation.")
+            await msg.reply_text(f"{user2.full_name} opted out of reputations,"
+                                 f" so you are not able to change their reputation.")
             return
         if user1.id != user2.id:
             LOGGER.debug(f"{user2.id} : {sql.get_reputation(chat.id, user2.id)}")
             sql.increase_reputation(chat.id, user2.id)
-            new_msg = msg.reply_text(f"<b>{user1.first_name}</b> ({sql.get_reputation(chat.id, user1.id)}) has increased reputation of <b>{user2.first_name}</b> ({sql.get_reputation(chat.id, user2.id)})", parse_mode=ParseMode.HTML).message_id
+            new_msg = await msg.reply_text(f"<b>{user1.first_name}</b> ({sql.get_reputation(chat.id, user1.id)}) has increased reputation of <b>{user2.first_name}</b> ({sql.get_reputation(chat.id, user2.id)})", parse_mode=ParseMode.HTML).message_id
             try:
-                context.bot.delete_message(chat.id, sql.get_latest_rep_message(chat.id))
+                await context.bot.delete_message(chat.id, sql.get_latest_rep_message(chat.id))
             except BadRequest as err:
                 LOGGER.debug("Could not delete that message.")
             sql.set_latest_rep_message(chat.id, new_msg)
 
 
-async def decrease(update: Update, context: CallbackContext):
+async def decrease(update: Update, context: ContextTypes.DEFAULT_TYPE):
     msg = update.effective_message
     user1 = update.effective_user
     chat = update.effective_chat
@@ -45,26 +45,26 @@ async def decrease(update: Update, context: CallbackContext):
     if msg.reply_to_message:
         user2 = msg.reply_to_message.from_user
         if not settings.user_should_reputate(user1.id):
-            msg.reply_text("You have opted out of reputations, so you are not able to change others reputation neither.")
+            await msg.reply_text("You have opted out of reputations, so you are not able to change others' reputation neither.")
             return
         if not settings.user_should_reputate(user2.id):
-            msg.reply_text(f"{user2.full_name} opted out of reputations,"
-                           f" so you are not able to change their reputation.")
+            await msg.reply_text(f"{user2.full_name} opted out of reputations,"
+                                 f" so you are not able to change their reputation.")
             return
         if user1.id != user2.id:
             LOGGER.debug(f"{user2.id} : {sql.get_reputation(chat.id, user2.id)}")
             sql.decrease_reputation(chat.id, user2.id)
-            new_msg = msg.reply_text(
+            new_msg = await msg.reply_text(
                 f"<b>{user1.first_name}</b> ({sql.get_reputation(chat.id, user1.id)}) has decreased reputation of <b>{user2.first_name}</b> ({sql.get_reputation(chat.id, user2.id)})",
                 parse_mode=ParseMode.HTML).message_id
             try:
-                context.bot.delete_message(chat.id, sql.get_latest_rep_message(chat.id))
+                await context.bot.delete_message(chat.id, sql.get_latest_rep_message(chat.id))
             except BadRequest as err:
                 LOGGER.debug("Could not delete that message.")
             sql.set_latest_rep_message(chat.id, new_msg)
 
 
-async def reputation(update: Update, context: CallbackContext):
+async def reputation(update: Update, context: ContextTypes.DEFAULT_TYPE):
     args = context.args
     chat = update.effective_chat  # type: Optional[Chat]
     msg = update.effective_message  # type: Optional[Message]
@@ -73,28 +73,33 @@ async def reputation(update: Update, context: CallbackContext):
         if len(args) >= 1:
             if args[0] in ("yes", "on", "true"):
                 settings.set_user_setting(chat.id, True)
-                msg.reply_text("Turned on reputation!")
+                await msg.reply_text("Turned on reputation!")
 
             elif args[0] in ("no", "off", "false"):
                 settings.set_user_setting(chat.id, False)
-                msg.reply_text("Turned off reputation!")
+                await msg.reply_text("Turned off reputation!")
         else:
-            msg.reply_text("Your current reputation preference is: `{}`".format(settings.user_should_reputate(chat.id)),
-                           parse_mode=ParseMode.MARKDOWN)
+            await msg.reply_text("Your current reputation preference is: `{}`".format(settings.user_should_reputate(chat.id)),
+                                parse_mode=ParseMode.MARKDOWN)
 
     else:
         if len(args) >= 1:
             if args[0] in ("yes", "on"):
                 settings.set_chat_setting(chat.id, True)
-                msg.reply_text("Turned on reputation! Users will now be able to vote on each others messages, except "
-                               "if they opted out.")
+                await msg.reply_text("Turned on reputation! Users will now be able to vote on each other's messages, except "
+                                     "if they opted out.")
 
             elif args[0] in ("no", "off"):
                 settings.set_chat_setting(chat.id, False)
-                msg.reply_text("Turned off reputation!")
+                await msg.reply_text("Turned off reputation!")
         else:
-            msg.reply_text("This chat's current setting is: `{}`".format(settings.chat_should_reputate(chat.id)),
-                           parse_mode=ParseMode.MARKDOWN)
+            await msg.reply_text("This chat's current setting is: `{}`".format(settings.chat_should_reputate(chat.id)),
+                                parse_mode=ParseMode.MARKDOWN)
+
+
+def __migrate__(old_chat_id, new_chat_id):
+    await sql.migrate_chat_latest_messages(old_chat_id, new_chat_id)
+
 
 
 def __help__(update: Update) -> str:
@@ -114,23 +119,23 @@ def __user_settings__(user_id):
         settings.user_should_reputate(user_id))
 
 
-INCREASE_MESSAGE_HANDLER = DisableAbleMessageHandler(Filters.regex(r"^\+$"), increase, friendly="increase",
-                                                     run_async=True)
+INCREASE_MESSAGE_HANDLER = DisableAbleMessageHandler(
+    filters.Regex(r"^(?i)+(.*)$"), afk, friendly="afk", block=False
+)
+DECREASE_MESSAGE_HANDLER = DisableAbleMessageHandler(
+    filters.Regex(r"^(?i)-(.*)$"), afk, friendly="afk", block=False
+)
+INCREASE_MESSAGE_HANDLER2 = DisableAbleMessageHandler(
+    filters.Regex(r"^(?i)👍(.*)$"), afk, friendly="afk", block=False
+)
+DECREASE_MESSAGE_HANDLER2 = DisableAbleMessageHandler(
+    filters.Regex(r"^(?i)👎(.*)$"), afk, friendly="afk", block=False
+)
+SETTINGS_HANDLER = CommandHandler("reputation", reputation, block=False)
+
 
 application.add_handler(INCREASE_MESSAGE_HANDLER)
-DECREASE_MESSAGE_HANDLER = DisableAbleMessageHandler(Filters.regex(r"^\-$"), decrease, friendly="decrease",
-                                                     run_async=True)
-dispatcher.add_handler(DECREASE_MESSAGE_HANDLER)
-INCREASE_MESSAGE_HANDLER2 = DisableAbleMessageHandler(Filters.regex(r"^\👍$"), increase, friendly="increase",
-                                                     run_async=True)
-
+application.add_handler(DECREASE_MESSAGE_HANDLER)
 application.add_handler(INCREASE_MESSAGE_HANDLER2)
-DECREASE_MESSAGE_HANDLER2 = DisableAbleMessageHandler(Filters.regex(r"^\👎$"), decrease, friendly="decrease",
-                                                     run_async=True)
 application.add_handler(DECREASE_MESSAGE_HANDLER2)
-SETTINGS_HANDLER = CommandHandler("reputation", reputation, run_async=True)
 application.add_handler(SETTINGS_HANDLER)
-
-
-def __migrate__(old_chat_id, new_chat_id):
-    sql.migrate_chat_latest_messages(old_chat_id, new_chat_id)
