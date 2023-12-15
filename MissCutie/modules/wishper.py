9ba0bbc86479d815a0c1f6c6ee7879e157ffc240
Saif Ali <sachinsaif03@gmail.com>
2023-12-15 21:10:49 +0530

    Fix on mongodb

--- MissCutie/modules/wishper.py
+++ MissCutie/modules/wishper.py
@@ -1,107 +1,155 @@
-from datetime import datetime
-from uuid import uuid4
+import shortuuid
+from pymongo import MongoClient
+from telegram import (
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    InlineQueryResultArticle,
+    InputTextMessageContent,
+    Update,
+)
+from telegram.ext import CallbackQueryHandler, ContextTypes, InlineQueryHandler
 
-from telegram import Update, InlineQueryResultArticle, InputTextMessageContent, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.error import BadRequest
-from telegram.ext import ContextTypes, InlineQueryHandler, CallbackQueryHandler, ChosenInlineResultHandler
-from telegram.constants import ParseMode
+from MissCutie import MONGO_DB_URI, application
 
-from MissCutie import LOGGER, application
-from MissCutie.modules.users import get_user_id
-import MissCutie.modules.sql.whisper_sql as sql
-
-
-class Whisper():
-    receiver = 0
-    message = 0
-
-    def __init__(self, receiver, message):
-        self.message = message
-        self.receiver = receiver
-
-    def to_string(self):
-        return str(self.receiver.id) + str(self.message)
+# Initialize MongoDB client
+client = MongoClient(MONGO_DB_URI)
+db = client["whispers"]
+collection = db
 
 
-LIST = []
+
+class Whispers:
+    @staticmethod
+    def add_whisper(WhisperId, WhisperData):
+        whisper = {"WhisperId": WhisperId, "whisperData": WhisperData}
+        collection.insert_one(whisper)
+
+    @staticmethod
+    def del_whisper(WhisperId):
+        collection.delete_one({"WhisperId": WhisperId})
+
+    @staticmethod
+    def get_whisper(WhisperId):
+        whisper = collection.find_one({"WhisperId": WhisperId})
+        return whisper["whisperData"] if whisper else None
 
 
-async def chosen_inline_button(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    result = update.chosen_inline_result
-    query = result.query
-    q = query.split(" ")
-    username = q[-1]
-    receiver_id = get_user_id(username)
-    sender_id = update.effective_user.id
-    text = ""
-    for element in q:
-        if element != username:
-            text = text + " " + element
-    sql.add_whisper(sender_id, receiver_id, text, sql.get_whispers(context.bot.id))
-    sql.increase_whisper_ids(context.bot.id)
 
+async def mainwhisper(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.inline_query
+    if not query.query:
+        return await query.answer(
+            [],
+            switch_pm_text="Give me a username or ID!",
+            switch_pm_parameter="ghelp_whisper",
+        )
 
-async def button(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    query = update.callback_query
-    print(query)
-    id = query.data.replace("whisper_", "")
-    whisper_message = sql.get_message(int(id))
-    sender = whisper_message.sender_id
-    receiver = whisper_message.receiver_id
-    message = whisper_message.message
-    if update.effective_user.id not in (sender, receiver):
-        await context.bot.answer_callback_query(update.callback_query.id,
-                                                "You are not permitted to read this message.",
-                                                show_alert=False)
+    user, message = parse_user_message(query.query)
+    if len(message) > 200:
         return
-    await context.bot.answer_callback_query(update.callback_query.id, message, show_alert=True)
+
+    usertype = "username" if user.startswith("@") else "id"
+
+    if user.isdigit():
+        try:
+            chat = await context.bot.get_chat(int(user))
+            user = f"@{chat.username}" if chat.username else chat.first_name
+        except Exception:
+            pass
+
+    whisperData = {
+        "user": query.from_user.id,
+        "withuser": user,
+        "usertype": usertype,
+        "type": "inline",
+        "message": message,
+    }
+    whisperId = shortuuid.uuid()
+
+    # Add the whisper to the database
+    Whispers.add_whisper(whisperId, whisperData)
+
+    answers = [
+        InlineQueryResultArticle(
+            id=whisperId,
+            title=f"👤 Send a whisper message to {user}!",
+            description="Only they can see it!",
+            input_message_content=InputTextMessageContent(
+                f"🔐 A Whisper Message For {user}\nOnly they can see it!"
+            ),
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(
+                            "📩 Show Message 📩",
+                            callback_data=f"whisper_{whisperId}",
+                        )
+                    ]
+                ]
+            ),
+        )
+    ]
+
+    await context.bot.answer_inline_query(query.id, answers)
 
 
-async def process_inline_query(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    user = update.effective_user
-    query = update.inline_query.query
-    results = []
-    q = query.split(" ")
-    username = q[-1]
-    if not username.startswith("@"):
-        results.append(InlineQueryResultArticle(
-            id=uuid4(),
-            title="This does not work.",
-            description="You need to specify the user you want to message. If this person does not have one, you can not whisper to them.",
-            input_message_content=InputTextMessageContent("Write targets @username at the end of your message in order to send a message.", ParseMode.MARKDOWN)
-        ))
+# Callback query handler
+async def showWhisper(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    callback_query = update.callback_query
+    whisperId = callback_query.data.split("_")[-1]
+    whisper = Whispers.get_whisper(whisperId)
+
+    if not whisper:
+        await context.bot.answer_callback_query(
+            callback_query.id, "This whisper is not valid anymore!"
+        )
+        return
+
+    userType = whisper["usertype"]
+    from_user_id = callback_query.from_user.id
+
+    if from_user_id == whisper["user"]:
+        await context.bot.answer_callback_query(
+            callback_query.id, whisper["message"], show_alert=True
+        )
+    elif (
+        userType == "username"
+        and callback_query.from_user.username
+        and callback_query.from_user.username.lower()
+        == whisper["withuser"].replace("@", "").lower()
+    ):
+        await context.bot.answer_callback_query(
+            callback_query.id, whisper["message"], show_alert=True
+        )
+    elif userType == "id" and from_user_id == int(whisper["withuser"]):
+        await context.bot.answer_callback_query(
+            callback_query.id, whisper["message"], show_alert=True
+        )
     else:
-        for element in q:
-            if element != username:
-                try:
-                    current_time = datetime.now()
-                    receiver = context.bot.get_chat(get_user_id(username))
-                    name = receiver.first_name
-                    title = "A whisper message to " + name
-                    print("Title: " + title)
-                    results.append(InlineQueryResultArticle(
-                        id=uuid4(),
-                        title=title,
-                        description="Only they can open it.",
-                        input_message_content=InputTextMessageContent(f"🔒 A whisper message to @{receiver.username}, Only they can open it."),
-                        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(
-                            text="show message",
-                            callback_data="whisper_" + str(sql.get_whispers(context.bot.id)))
-                        ]])
-                    ))
-
-                except BadRequest as excp:
-                    if excp.message == 'Chat not found':
-                        pass
-                    else:
-                        LOGGER.exception("Error extracting user ID")
-    await update.inline_query.answer(results)
+        await context.bot.answer_callback_query(
+            callback_query.id, "Not your Whisper!", show_alert=True
+        )
 
 
-QUERY_HANDLER = InlineQueryHandler(process_inline_query, block=False)
-BUTTON_HANDLER = CallbackQueryHandler(button, pattern=r"whisper", block=False)
-INLINE_RESULT_HANDLER = ChosenInlineResultHandler(chosen_inline_button, block=False)
+# Function to parse user message
+def parse_user_message(query_text):
+    text = query_text.split(" ")
+    user = text[0]
+    first = True
+    message = ""
 
-application.add_handler(QUERY_HANDLER)
-application.add_handler(BUTTON_HANDLER)
-application.add_handler(INLINE_RESULT_HANDLER)
+    if not user.startswith("@") and not user.isdigit():
+        user = text[-1]
+        first = False
+
+    if first:
+        message = " ".join(text[1:])
+    else:
+        text.pop()
+        message = " ".join(text)
+
+    return user, message
+
+
+application.add_handler(InlineQueryHandler(mainwhisper, block=False))
+application.add_handler(CallbackQueryHandler(showWhisper, pattern="^whisper_", block=False))
