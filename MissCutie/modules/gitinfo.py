60dea9f301e48d31120669fd85bf7bde2c6e29df
Saif Ali <sachinsaif03@gmail.com>
2023-12-29 02:29:45 +0530

    Updated

--- MissCutie/modules/gitinfo.py
+++ MissCutie/modules/gitinfo.py
@@ -1,6 +1,6 @@
 import requests
 from telegram.constants import ParseMode
-from telegram.ext import ContextTypes, CommandHandler, filters
+from telegram.ext import ContextTypes, CommandHandler
 from telegram import Update
 from MissCutie import application
 
@@ -13,23 +13,29 @@ async def github(update: Update, context: ContextTypes.DEFAULT_TYPE):
     URL = f"https://api.github.com/users/{username}"
 
     try:
-        response = await requests.get(URL)
+        response = requests.get(URL)
         response.raise_for_status()
-        result = await response.json()
 
-        url = result.get("html_url")
-        name = result.get("name", "")
-        company = result.get("company", "")
-        bio = result.get("bio", "")
-        created_at = result.get("created_at", "")
-        avatar_url = result.get("avatar_url", "")
-        blog = result.get("blog", "")
-        location = result.get("location", "")
-        repositories = result.get("public_repos", 0)
-        followers = result.get("followers", 0)
-        following = result.get("following", 0)
+        try:
+            result = response.json()
+        except ValueError:
+            await update.message.reply_text("Error: Unable to parse GitHub API response")
+            return
 
-        caption = f"""**Info Of {name}**
+        if isinstance(result, dict):
+            url = result.get("html_url")
+            name = result.get("name", "")
+            company = result.get("company", "")
+            bio = result.get("bio", "")
+            created_at = result.get("created_at", "")
+            avatar_url = result.get("avatar_url", "")
+            blog = result.get("blog", "")
+            location = result.get("location", "")
+            repositories = result.get("public_repos", 0)
+            followers = result.get("followers", 0)
+            following = result.get("following", 0)
+
+            caption = f"""**Info Of {name}**
 **Username :** `{username}`
 **Bio :** `{bio}`
 **Profile link :** [{name}]({url})
@@ -41,13 +47,14 @@ async def github(update: Update, context: ContextTypes.DEFAULT_TYPE):
 **Followers  :** `{followers}`
 **Following :** `{following}`"""
 
-        await update.message.reply_photo(photo=avatar_url, caption=caption, parse_mode=ParseMode.MARKDOWN)
+            await update.message.reply_photo(photo=avatar_url, caption=caption, parse_mode=ParseMode.MARKDOWN)
+        else:
+            await update.message.reply_text("Error: Unexpected GitHub API response")
 
     except requests.exceptions.HTTPError as e:
         if e.response.status_code == 404:
             await update.message.reply_text("No Github Account Found For This Username")
         else:
             print(f"Error: {e}")
-
 
 application.add_handler(CommandHandler("github", github))
