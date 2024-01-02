a018200e9272caf0f62c2007a714a537717277fa
Saif Ali <sachinsaif03@gmail.com>
2024-01-02 06:54:29 +0530

    Update __init__.py

--- Database/mongodb/__init__.py
+++ Database/mongodb/__init__.py
@@ -5,4 +5,4 @@ from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
 from MissCutie import MONGO_DB_URI
 
 mongo = MongoClient(MONGO_DB_URI)
-db = mongo.MissCutie
+dbname = mongo.MissCutie
