586adb0adb3ca9816c8eeed69d83ad3f16c96b0b
Saif Ali <sachinsaif03@gmail.com>
2024-01-03 13:15:05 +0530

    Update

--- MissCutie/config.py
+++ MissCutie/config.py
@@ -1,7 +1,6 @@
-# Create a new config.py or rename this to config.py file in same dir and import, then extend this class.
 import json
-import os
 import logging
+import os
 
 
 def get_user_list(config, key):
@@ -9,67 +8,51 @@ def get_user_list(config, key):
         return json.load(json_file)[key]
 
 
-# Create a new config.py or rename this to config.py file in same dir and import, then extend this class.
+# Create a new config.py or rename this to config.py file in the same dir and import, then extend this class.
 class Config(object):
-    LOGGER = True
-    # REQUIRED
-    # Login to https://my.telegram.org and fill in these slots with the details given by it
-
-    API_ID = 23738177  # integer value, dont use ""
+    ALLOW_CHATS = True
+    ALLOW_EXCL = True
     API_HASH = "d54a6609d5cc5ba0094ee206791e1490"
-    TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 5982904247  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "LostedPerson"
-    SUPPORT_CHAT = "PublicSource_Chat"  # Your own group for support, do not add the @
-    JOIN_LOGGER = (
-        -1001660668029
-    )  # Prints any new group the bot is added to, prints just the name and ID.
-    EVENT_LOGS = (
-        -1001660668029
-    )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
-
-    # RECOMMENDED
-    SQLALCHEMY_DATABASE_URI = "something://somewhat:user@hosturl:port/databasename"  # needed for any database modules
-    MONGO_DB_URI = "MONGO_DB_URI"
+    API_ID = 23738177  # integer value, don't use ""
     ARQ_API_KEY = "TLKINQ-XEVTPG-FQPEVU-ODUYVW-ARQ"
-    OPENAI_API_KEY = "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"
-    LOAD = []
-    NO_LOAD = None
-    WEBHOOK = False
-    INFOPIC = True
-    URL = None
-
-    # OPTIONAL
-    ##List of id's -  (not usernames) for users which have sudo access to the bot.
-    DRAGONS = get_user_list("elevated_users.json", "sudos")
-    ##List of id's - (not usernames) for developers who will have the same perms as the owner
-    DEV_USERS = get_user_list("elevated_users.json", "devs")
-    ##List of id's (not usernames) for users which are allowed to gban, but can also be banned.
-    DONATION_LINK = None  # EG, paypal
-    CERT_PATH = None
-    PORT = 5000
-    DEL_CMDS = True  # Delete commands that users dont have access to, like delete /ban if a non admin uses it.
-    STRICT_GBAN = True
-    WORKERS = (
-        8  # Number of subthreads to use. Set as number of threads your processor uses
-    )
     BAN_STICKER = ""  # banhammer marie sticker id, the bot will send this sticker before banning or kicking a user in chat.
-    ALLOW_EXCL = True  # Allow ! commands as well as / (Leave this to true so that blacklist can work)
+    CERT_PATH = None
+    DEL_CMDS = True  # Delete commands that users don't have access to, like delete /ban if a non-admin uses it.
+    DEV_USERS = get_user_list("elevated_users.json", "devs")
+    DONATION_LINK = None  # EG, PayPal
+    DRAGONS = get_user_list("elevated_users.json", "sudos")
+    EVENT_LOGS = -1001660668029
+    INFOPIC = True
+    JOIN_LOGGER = -1001660668029
+    LOAD = []
+    LOGGER = True
+    LOGGER_LEVEL = logging.INFO
+    MONGO_DB_URI = "MONGO_DB_URI"
+    MONGO_DB_NAME = "Cutie"
+    NO_LOAD = None
+    OPENAI_API_KEY = "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"
+    OWNER_ID = 5982904247  # If you don't know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "LostedPerson"
+    PORT = 5000
+    SQLALCHEMY_DATABASE_URI = "something://somewhat:user@hosturl:port/databasename"  # needed for any database modules
+    STRICT_GBAN = True
+    SUPPORT_CHAT = "PublicSource_Chat"  # Your own group for support, do not add the @
+    TELETHON_VERSION = "1.0.0"
+    TEMP_DOWNLOAD_LOC = "./Downloads"
     TIME_API_KEY = "awoo"  # Get your API key from https://timezonedb.com/api
-    WALL_API = (
-        "awoo"  # For wallpapers, get one from https://wall.alphacoders.com/api.php
-    )
-    AI_API_KEY = "awoo"  # For chatbot, get one from https://coffeehouse.intellivoid.net/dashboard
+    TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    URL = None
+    WEBHOOK = False
+    WALL_API = "awoo"  # For wallpapers, get one from https://wall.alphacoders.com/api.php
+    WORKERS = 8  # Number of subthreads to use. Set as the number of threads your processor uses
+
     BL_CHATS = []  # List of groups that you want blacklisted.
     SPAMMERS = None
-    ALLOW_CHATS = True
-    LOGGER_LEVEL = logging.INFO
-
-    TEMP_DOWNLOAD_LOC = "./Downloads"
-
-class Production(Config):
-    LOGGER = True
 
 
 class Development(Config):
+    LOGGER = True
+
+
+class Production(Config):
     LOGGER = True
