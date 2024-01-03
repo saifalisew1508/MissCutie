d7d7abe435030aedd3cf7f26924c7ca5217202ef
Saif Ali <sachinsaif03@gmail.com>
2024-01-03 12:53:44 +0530

    Update __init__.py

--- Database/mongodb/__init__.py
+++ Database/mongodb/__init__.py
@@ -2,7 +2,7 @@ from typing import Dict, Union
 
 from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
 
-from MissCutie import MONGO_DB_URI
+from MissCutie import DB_NAME, MONGO_DB_URI
 
 client = MongoClient(MONGO_DB_URI)
 mongo = client[DB_NAME]
