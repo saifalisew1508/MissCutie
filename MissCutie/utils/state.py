cc44af1976e508c45f86a4d41eb6fe2df27de3f3
Saif Ali <sachinsaif03@gmail.com>
2024-01-05 07:10:12 +0530

    Create state.py

---
+++ MissCutie/utils/state.py
@@ -0,0 +1,25 @@
+from aiohttp import ClientSession
+from httpx import AsyncClient, Timeout
+from Python_ARQ import ARQ
+
+
+# Aiohttp Async Client
+session = ClientSession()
+
+# HTTPx Async Client
+state = AsyncClient(
+    http2=True,
+    verify=False,
+    headers={
+        "Accept-Language": "en-US,en;q=0.9,id-ID;q=0.8,id;q=0.7",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edge/107.0.1418.42",
+    },
+    timeout=Timeout(20),
+)
+
+
+
+ARQ_API_KEY = "RLWCED-WZASYO-AWOLTB-ITBWTP-ARQ"  # GET API KEY FROM @ARQRobot
+ARQ_API_URL = "arq.hamker.dev"
+
+arq = ARQ(ARQ_API_URL, ARQ_API_KEY, session)
