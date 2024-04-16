161bdd841504c8f49fcf77514115a2193f199fc5
Saif Ali <sachinsaif03@gmail.com>
2024-04-16 14:12:02 +0530

    Update

--- MissCutie/modules/copyright.py
+++ MissCutie/modules/copyright.py
@@ -1,23 +1,27 @@
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
-import time
+from datetime import datetime
 from MissCutie import application, OWNER_ID
-from Database.mongodb.copyright import enable_anticopyright, disable_anticopyright, is_anticopyright_enabled
+from Database.mongodb.copyright import (
+    enable_anticopyright,
+    disable_anticopyright,
+    is_anticopyright_enabled,
+)
 
-# Define the callback function to handle messages
-async def message_handler(update: Update, context: CallbackContext) -> None:
+async def handle_message(update: Update, context: CallbackContext) -> None:
     message = update.message
     chat_id = message.chat_id
-
-    # Check if the chat has enabled anticopyright
     anticopyright_enabled = is_anticopyright_enabled(chat_id)
 
     if anticopyright_enabled:
-        # Check if the message is edited or contains stickers or videos
-        if (message.edit_date or message.sticker or message.video) and message.date < time.time() - 120:
-            message.delete()
+        if (
+            message.edit_date
+            or message.sticker
+            or message.video
+            and message.date < datetime.now().timestamp() - 120
+        ):
+            await message.delete()
 
-# Define the command handler to enable anticopyright
 async def set_anticopyright(update: Update, context: ContextTypes.DEFAULT_TYPE):
     message = update.effective_message
     chat = update.effective_chat
@@ -26,46 +30,36 @@ async def set_anticopyright(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     if len(args) > 0:
         s = args[0].lower()
-
         if s in ["yes", "on", "true"]:
             enable_anticopyright(chat.id)
             await message.reply_html(
-                "Anticopyright has been enabled in {}\nI will delete edited messages, stickers, and videos after 2 minutes".format(
-                    html.escape(chat.title)))
+                f"Anticopyright has been enabled in {html.escape(chat.title)}\nI will delete edited messages, stickers, and videos after 2 minutes"
+            )
             log_message = (
                 f"#ANTICOPYRIGHT\n"
                 f"Enabled\n"
                 f"<b>Admin:</b> {mention_html(user.id, user.first_name)}"
             )
             return log_message
-
         elif s in ["off", "no", "false"]:
             disable_anticopyright(chat.id)
             await message.reply_html(
-                "Anticopyright has been disabled in {}\nI will no longer delete edited messages, stickers, and videos after 2 minutes".format(
-                    html.escape(chat.title)))
+                f"Anticopyright has been disabled in {html.escape(chat.title)}\nI will no longer delete edited messages, stickers, and videos after 2 minutes"
+            )
             log_message = (
                 f"#ANTICOPYRIGHT\n"
                 f"Disabled\n"
                 f"<b>Admin:</b> {mention_html(user.id, user.first_name)}"
             )
             return log_message
-
         else:
-            await message.reply_text("Unrecognized arguments {}".format(s))
+            await message.reply_text(f"Unrecognized arguments {s}")
             return
-
     await message.reply_html(
-        "Anticopyright setting is currently <b><i>{}</i></b> in <code>{}</code>\n\n"
-        "When this setting is on, I will delete edited messages, stickers, and videos after 2 minutes".format(
-            anticopyright_status(chat.id), html.escape(chat.title)))
+        f"Anticopyright setting is currently <b><i>{anticopyright_status(chat.id)}</i></b> in <code>{html.escape(chat.title)}</code>\n\n"
+        "When this setting is on, I will delete edited messages, stickers, and videos after 2 minutes"
+    )
     return
 
-
-
-# Add handlers
-application.add_handler(CommandHandler('anticopyright, set_anticopyright))
-application.add_handler(MessageHandler(Filters.all, message_handler))
-
-
-
+application.add_handler(CommandHandler("anticopyright", set_anticopyright))
+application.add_handler(MessageHandler(Filters.all, handle_message))
