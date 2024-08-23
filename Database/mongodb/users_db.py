f449083e22c400dcc26a3b67ee6e19aafa65af46
Saif Ali <sachinsaif03@gmail.com>
2024-08-24 03:34:40 +0530

    Update users_db.py

--- Database/mongodb/users_db.py
+++ Database/mongodb/users_db.py
@@ -2,7 +2,7 @@ from threading import RLock
 from time import time
 
 from Database.mongodb.mongodb import MongoDB
-from Mikobot import LOGGER
+from MissCutie import LOGGER
 
 INSERTION_LOCK = RLock()
 
